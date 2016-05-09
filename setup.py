6936d9f01816a9afc0f623de968afddc338d1716
Magnus Edenhill <magnus@confluent.io>
2016-05-09 22:56:46 +0200

    Moved sources to src/ to avoid cimpl package collision

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 from distutils.core import Extension
 
 
 module = Extension('confluent_kafka.cimpl',
                     include_dirs = ['/usr/local/include'],
                     libraries= ['rdkafka'],
-                    sources=['confluent_kafka/cimpl/confluent_kafka.c', 'confluent_kafka/cimpl/Producer.c', 'confluent_kafka/cimpl/Consumer.c'])
+                    sources=['confluent_kafka/src/confluent_kafka.c',
+                             'confluent_kafka/src/Producer.c',
+                             'confluent_kafka/src/Consumer.c'])
 
-setup (name='confluent-kafka',
-       version='0.9.1',
-       description='Confluent\'s Apache Kafka client for Python',
-       author='Confluent Inc',
-       author_email='support@confluent.io',
-       url='https://github.com/confluentinc/confluent-kafka-python',
-       ext_modules=[module],
-       packages=['confluent_kafka', 'confluent_kafka.cimpl', 'confluent_kafka.kafkatest'])
+setup(name='confluent-kafka',
+      version='0.9.1',
+      description='Confluent\'s Apache Kafka client for Python',
+      author='Confluent Inc',
+      author_email='support@confluent.io',
+      url='https://github.com/confluentinc/confluent-kafka-python',
+      ext_modules=[module],
+      packages=find_packages())
