20bba1cc9c6586eaf84cf96bd938ff4e78f966f3
Magnus Edenhill <magnus@confluent.io>
2016-08-08 23:19:57 +0200

    Review cleanups: Py2 support, PEP8 clean

--- tests/test_threads.py
+++ tests/test_threads.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
 from confluent_kafka import Producer, KafkaError, KafkaException
-import threading, time, queue
+import threading
+import time
+try:
+    from queue import Queue, Empty
+except:
+    from Queue import Queue, Empty
 
 
 class IntendedException (Exception):
     pass
 
-def thread_run (myid,p,q):
-    def do_crash (err, msg):
+
+def thread_run(myid, p, q):
+    def do_crash(err, msg):
         raise IntendedException()
 
     for i in range(1, 3):
@@ -23,38 +29,34 @@ def thread_run (myid,p,q):
         except IntendedException:
             print(myid, "Intentional callback crash: ok")
             continue
-        except:
-            raise
 
     print(myid, 'Done')
     q.put(myid)
 
-    
 
 def test_thread_safety():
     """ Basic thread safety tests. """
 
-    q = queue.Queue()
-    p = Producer({'socket.timeout.ms':10,
+    q = Queue()
+    p = Producer({'socket.timeout.ms': 10,
                   'socket.blocking.max.ms': 10,
                   'default.topic.config': {'message.timeout.ms': 10}})
 
     threads = list()
     for i in range(1, 5):
-        thr = threading.Thread(target=thread_run, name=str(i), args=[i,p,q])
+        thr = threading.Thread(target=thread_run, name=str(i), args=[i, p, q])
         thr.start()
         threads.append(thr)
 
     for thr in threads:
         thr.join()
 
-
     # Count the number of threads that exited cleanly
     cnt = 0
     try:
         for x in iter(q.get_nowait, None):
             cnt += 1
-    except queue.Empty:
+    except Empty:
         pass
 
     if cnt != len(threads):
@@ -62,6 +64,6 @@ def test_thread_safety():
 
     print('Done')
 
-    
-if __name__=='__main__':
+
+if __name__ == '__main__':
     test_thread_safety()
