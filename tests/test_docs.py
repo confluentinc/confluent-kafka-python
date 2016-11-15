6c00e7aa8ff003e366cbd773cc65dba2ec617a91
Magnus Edenhill <magnus@edenhill.se>
2016-11-15 09:43:54 +0100

    test_docs: improved error reporting

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -14,16 +14,23 @@ def test_verify_docs():
             # Skip internals
             continue
 
+        err = None
+        level = 'ERROR'
         o = confluent_kafka.__dict__.get(n)
         d = o.__doc__
         if not d:
-            print('Missing __doc__ for: %s (type %s)' % (n, type(o)))
+            err = 'Missing __doc__ for: %s (type %s)' % (n, type(o))
             fails += 1
         elif not re.search(r':', d):
-            print('Missing Doxygen tag for: %s (type %s)' % (n, type(o)))
+            err = 'Missing Doxygen tag for: %s (type %s)' % (n, type(o))
             # Ignore missing doc strings for the cimpl module itself and
             # integer constants (which can't have a doc string)
-            if n != 'cimpl' and type(o) not in [int]:
+            if n == 'cimpl' or type(o) in [int]:
+                level = 'IGNORE'
+
+        if err is not None:
+            print('%s: %s' % (level, err))
+            if level == 'ERROR':
                 fails += 1
 
     assert fails == 0
