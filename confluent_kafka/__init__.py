956ff94f090b19e60b057ec3eae6c6243deace05
co-dh <hao@datacratic.com>
2016-11-29 09:46:36 -0500

    replace from .cimpl import * with explicit names. (#87, @co-dh)
    * pylint complains there is no Consumer in confluent_kafka
    
    better explicit on what's imported than *.
    
    * Update __init__.py
    
    * added enums

--- confluent_kafka/__init__.py
+++ confluent_kafka/__init__.py
@@ -1,2 +1,3 @@
 __all__ = ['cimpl', 'avro', 'kafkatest']
-from .cimpl import *
+from .cimpl import (Consumer, KafkaError, KafkaException, Message, Producer, TopicPartition, libversion, version, 
+                    TIMESTAMP_NOT_AVAILABLE, TIMESTAMP_CREATE_TIME, TIMESTAMP_LOG_APPEND_TIME)
