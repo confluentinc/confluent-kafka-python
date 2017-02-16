d415bb90216cb5bacb635b60514102f0a1e81b13
Alex Ayars <alex.ayars@confluent.io>
2017-02-15 17:35:39 -0800

    Bump version to 0.9.4
      - Automatic commit via:
        pint set-version --branch master --version 0.9.4 -e prod confluent-kafka-python --no-dry-run

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ module = Extension('confluent_kafka.cimpl',
                              'confluent_kafka/src/Consumer.c'])
 
 setup(name='confluent-kafka',
-      version='0.9.2',
+      version='0.9.4',
       description='Confluent\'s Apache Kafka client for Python',
       author='Confluent Inc',
       author_email='support@confluent.io',
