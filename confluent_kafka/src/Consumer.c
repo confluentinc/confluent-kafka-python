d5a046a7bf4ca8c1e8c852b902f84e89de5bdc65
John Dennison <dennison.john@gmail.com>
2017-10-03 01:12:57 +0000

    Makes calling consumer.close() multiple times safe. Throws a RuntimeError not a segfaults

--- confluent_kafka/src/Consumer.c
+++ confluent_kafka/src/Consumer.c
@@ -550,17 +550,24 @@ static PyObject *Consumer_poll (Handle *self, PyObject *args,
 
 
 static PyObject *Consumer_close (Handle *self, PyObject *ignore) {
-	CallState cs;
 
-	CallState_begin(self, &cs);
+	if (!self->rk) {
+            PyErr_SetString(PyExc_RuntimeError,
+                            "Consumer already closed");
+            return NULL;
+    }
 
-	rd_kafka_consumer_close(self->rk);
+    CallState cs;
 
-	rd_kafka_destroy(self->rk);
-	self->rk = NULL;
+    CallState_begin(self, &cs);
 
-        if (!CallState_end(self, &cs))
-                return NULL;
+    rd_kafka_consumer_close(self->rk);
+
+    rd_kafka_destroy(self->rk);
+    self->rk = NULL;
+
+    if (!CallState_end(self, &cs))
+            return NULL;
 
 	Py_RETURN_NONE;
 }
