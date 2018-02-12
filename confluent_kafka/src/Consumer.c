4a5b7299c34c97edcaf17a2bc7c296113a6526de
John Dennison <dennison.john@gmail.com>
2018-02-12 16:16:21 +0000

    fix consumer.consume header support

--- confluent_kafka/src/Consumer.c
+++ confluent_kafka/src/Consumer.c
@@ -951,7 +951,7 @@ static PyObject *Consumer_consume (Handle *self, PyObject *args,
                 PyObject *msgobj = Message_new0(self, rkmessages[i]);
                 // Have to deatch headers outside Message_new0 because it declares the
                 // rk message as a const
-                rd_kafka_message_detach_headers(rkm, &((Message *)msgobj)->c_headers);
+                rd_kafka_message_detach_headers(rkmessages[i], &((Message *)msgobj)->c_headers);
                 PyList_SET_ITEM(msglist, i, msgobj);
                 rd_kafka_message_destroy(rkmessages[i]);
         }
