b8ced77cd9935b1a0745b9826eb2c65e5040c87f
Magnus Edenhill <magnus@edenhill.se>
2018-10-03 19:24:08 +0200

    produce.offset.report=true is no longer needed

--- confluent_kafka/src/confluent_kafka.c
+++ confluent_kafka/src/confluent_kafka.c
@@ -1589,9 +1589,6 @@ rd_kafka_conf_t *common_conf_setup (rd_kafka_type_t ktype,
                 if (!common_conf_set_special(confdict, conf, "debug", vo))
                         goto outer_err;
 
-        /* Enable valid offsets in delivery reports */
-        rd_kafka_conf_set(conf, "produce.offset.report", "true", NULL, 0);
-
         /*
          * Plugins must be configured prior to handling any of their
          * configuration properties.
