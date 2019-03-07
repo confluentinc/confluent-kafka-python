c8b79bc939cce2a863f1e3503fdc9319fd5e64cf
Magnus Edenhill <magnus@edenhill.se>
2019-03-07 09:43:03 +0100

    Use librdkafka ./configure's auto installation of dependencies

--- tools/build-manylinux.sh
+++ tools/build-manylinux.sh
@@ -46,9 +46,6 @@ fi
 echo "# Installing basic system dependencies"
 yum install -y zlib-devel gcc-c++
 
-# Build OpenSSL
-$(dirname $0)/build-openssl.sh /usr
-
 echo "# Building librdkafka ${LIBRDKAFKA_VERSION}"
 $(dirname $0)/bootstrap-librdkafka.sh --require-ssl ${LIBRDKAFKA_VERSION} /usr
 
--- tools/prepare-cibuildwheel-linux.sh
+++ tools/prepare-cibuildwheel-linux.sh
@@ -15,9 +15,6 @@ set -ex
 echo "# Installing basic system dependencies"
 yum install -y zlib-devel gcc-c++
 
-# Build OpenSSL
-$(dirname $0)/build-openssl.sh /usr
-
 echo "# Building librdkafka ${LIBRDKAFKA_VERSION}"
 $(dirname $0)/bootstrap-librdkafka.sh --require-ssl ${LIBRDKAFKA_VERSION} /usr
 
