e5ea76970fc2873a4840b1866a575c0e8090659f
Magnus Edenhill <magnus@edenhill.se>
2019-03-07 09:43:03 +0100

    Use librdkafka ./configure's auto installation of dependencies

--- tools/build-linux-selfcontained.sh
+++ tools/build-linux-selfcontained.sh
@@ -78,13 +78,9 @@ LIBRDKAFKA_VERSION=$1
 function install_deps {
     echo "# Installing basic system dependencies"
     if which apt-get >/dev/null 2>&1; then
-        sudo apt-get -y install gcc g++ zlib1g-dev libssl-dev
+        sudo apt-get -y install gcc g++ zlib1g-dev
     else
         yum install -y zlib-devel gcc gcc-c++ libstdc++-devel
-
-        # Build OpenSSL
-        tools/build-openssl.sh /usr
-
     fi
 }
 
