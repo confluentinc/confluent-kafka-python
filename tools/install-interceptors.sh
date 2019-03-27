f6d4a8ed0abf087d592448839029f425b92e1ebd
rnpridgeon <ryan.n.pridgeon@gmail.com>
2019-03-27 14:34:43 -0400

    Revert confluent-librdkafka-plugins version bump

--- tools/install-interceptors.sh
+++ tools/install-interceptors.sh
@@ -16,10 +16,10 @@ set -e
 pkgtype=$1
 
 # Confluent Platform release version
-CPVER=5.3
+CPVER=5.0
 
 # confluent-librdkafka-plugins version
-PLUGINVER=0.11.1
+PLUGINVER=0.11.0
 
 # Stage directory for wheels
 stagedir=staging/libs
