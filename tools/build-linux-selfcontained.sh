ebe89975fb6fa1c8d13185e133fde53bf1a6e42a
rnpridgeon <ryan.n.pridgeon@gmail.com>
2019-03-27 21:47:33 -0400

    Add libsasl2-dev dependencies to build scripts

--- tools/build-linux-selfcontained.sh
+++ tools/build-linux-selfcontained.sh
@@ -78,9 +78,9 @@ LIBRDKAFKA_VERSION=$1
 function install_deps {
     echo "# Installing basic system dependencies"
     if which apt-get >/dev/null 2>&1; then
-        sudo apt-get -y install gcc g++ zlib1g-dev
+        sudo apt-get -y install gcc g++ zlib1g-dev libsasl2-dev
     else
-        yum install -y zlib-devel gcc gcc-c++ libstdc++-devel
+        yum install -y zlib-devel gcc gcc-c++ libstdc++-devel libsasl2-dev
     fi
 }
 
--- tools/prepare-cibuildwheel-linux.sh
+++ tools/prepare-cibuildwheel-linux.sh
@@ -13,7 +13,7 @@ fi
 set -ex
 
 echo "# Installing basic system dependencies"
-yum install -y zlib-devel gcc-c++
+yum install -y zlib-devel gcc-c++ libsasl2-dev
 
 echo "# Building librdkafka ${LIBRDKAFKA_VERSION}"
 $(dirname $0)/bootstrap-librdkafka.sh --require-ssl ${LIBRDKAFKA_VERSION} /usr
