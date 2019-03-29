767d51d9863757ff2e1cc92522da609c436453b0
rnpridgeon <ryan.n.pridgeon@gmail.com>
2019-03-28 20:07:06 -0400

    Remove lingering reference to cyrus-sasl

--- tools/prepare-cibuildwheel-linux.sh
+++ tools/prepare-cibuildwheel-linux.sh
@@ -13,7 +13,7 @@ fi
 set -ex
 
 echo "# Installing basic system dependencies"
-yum install -y zlib-devel gcc-c++ cyrus-sasl-devel
+yum install -y zlib-devel gcc-c++
 
 echo "# Building librdkafka ${LIBRDKAFKA_VERSION}"
 $(dirname $0)/bootstrap-librdkafka.sh --require-ssl ${LIBRDKAFKA_VERSION} /usr
