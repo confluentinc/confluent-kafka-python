485049cfa5afa79cf170e9f2ec01915b82293c70
rnpridgeon <ryan.n.pridgeon@gmail.com>
2019-11-05 05:10:24 -0500

    OpenSSL version bump to 1.0.2t

--- tools/build-openssl.sh
+++ tools/build-openssl.sh
@@ -6,7 +6,7 @@
 
 # NOTE: Keep this updated to make sure we always build the latest
 #       version of OpenSSL in the 1.0 release train.
-OPENSSL_VERSION=1.0.2s
+OPENSSL_VERSION=1.0.2t
 
 PREFIX=$1
 if [[ -z $PREFIX ]]; then
