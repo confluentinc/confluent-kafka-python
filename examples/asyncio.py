43dc4f71202c656a6c4da2917f5c436db07af7b7
Matt Howlett <matt.howlett@gmail.com>
2019-11-19 01:45:21 -0800

    Changes to produce implementations + flake8

--- examples/asyncio.py
+++ examples/asyncio.py
@@ -12,11 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Companion code to the blog post "Integrating Kafka With Python Asyncio Web Applications"
+# Companion code to the blog post "Integrating Kafka With Python
+# Asyncio Web Applications"
 # https://www.confluent.io/blog/[path-to-blog-post]
 
+# Example Siege [https://github.com/JoeDog/siege] test:
+# siege -c 400 -r 200 'http://localhost:8000/items1 POST {"name":"testuser"}'
+
 import asyncio
-from confluent_kafka import Producer
+import confluent_kafka
 from confluent_kafka import KafkaException
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
@@ -26,9 +30,9 @@ import uvicorn
 
 
 class AIOProducer:
-    def __init__(self, configs, loop):
+    def __init__(self, configs, loop=None):
         self._loop = loop or asyncio.get_event_loop()
-        self._producer = Producer(configs)
+        self._producer = confluent_kafka.Producer(configs)
         self._cancelled = False
         self._poll_thread = Thread(target=self._poll_loop)
         self._poll_thread.start()
@@ -41,8 +45,9 @@ class AIOProducer:
         self._cancelled = True
         self._poll_thread.join()
 
-    def async_produce(self, topic, value):
+    def produce(self, topic, value):
         result = self._loop.create_future()
+
         def ack(err, msg):
             if err:
                 self._loop.call_soon_threadsafe(result.set_exception, KafkaException(err))
@@ -51,60 +56,114 @@ class AIOProducer:
         self._producer.produce(topic, value, on_delivery=ack)
         return result
 
-    def produce(self, topic, value, ack=None):
-        self._producer.produce(topic, value, on_delivery=ack)
+    def produce2(self, topic, value, on_delivery):
+        result = self._loop.create_future()
 
+        def ack(err, msg):
+            if err:
+                self._loop.call_soon_threadsafe(
+                    result.set_exception, KafkaException(err))
+            else:
+                self._loop.call_soon_threadsafe(
+                    result.set_result, msg)
+            if on_delivery:
+                self._loop.call_soon_threadsafe(
+                    on_delivery, err, msg)
+        self._producer.produce(topic, value, on_delivery=ack)
+        return result
+
+
+class Producer:
+    def __init__(self, configs):
+        self._producer = confluent_kafka.Producer(configs)
+        self._cancelled = False
+        self._poll_thread = Thread(target=self._poll_loop)
+        self._poll_thread.start()
+
+    def _poll_loop(self):
+        while not self._cancelled:
+            self._producer.poll(0.1)
+
+    def close(self):
+        self._cancelled = True
+        self._poll_thread.join()
+
+    def produce(self, topic, value, on_delivery=None):
+        self._producer.produce(topic, value, on_delivery=on_delivery)
+
+
+config = {"bootstrap.servers": "localhost:9092"}
 
 app = FastAPI()
+
 
 class Item(BaseModel):
     name: str
 
+
+aio_producer = None
 producer = None
+
 
 @app.on_event("startup")
 async def startup_event():
-    global producer
-    producer = AIOProducer(
-        {"bootstrap.servers": "localhost:9092"},
-        asyncio.get_event_loop())
+    global producer, aio_producer
+    aio_producer = AIOProducer(config, asyncio.get_event_loop())
+    producer = Producer(config)
+
 
 @app.on_event("shutdown")
 def shutdown_event():
+    aio_producer.close()
     producer.close()
+
 
 @app.post("/items1")
 async def create_item1(item: Item):
     try:
-        result = await producer.async_produce("items", item.name)
-        return { "timestamp": result.timestamp() }
+        result = await aio_producer.produce("items", item.name)
+        return {"timestamp": result.timestamp()}
     except KafkaException as ex:
         raise HTTPException(status_code=500, detail=ex.args[0].str())
 
 cnt = 0
+
+
 def ack(err, msg):
     global cnt
     cnt = cnt + 1
 
+
 @app.post("/items2")
 async def create_item2(item: Item):
     try:
-        result = producer.produce("items", item.name, ack=ack)
-        return { "timestamp": time() }
+        aio_producer.produce2("items", item.name, on_delivery=ack)
+        return {"timestamp": time()}
     except KafkaException as ex:
         raise HTTPException(status_code=500, detail=ex.args[0].str())
+
 
 @app.post("/items3")
 async def create_item3(item: Item):
     try:
-        result = producer.produce("items", item.name)
-        return { "timestamp": time() }
+        producer.produce("items", item.name, on_delivery=ack)
+        return {"timestamp": time()}
     except KafkaException as ex:
         raise HTTPException(status_code=500, detail=ex.args[0].str())
 
+
 @app.post("/items4")
 async def create_item4(item: Item):
-    return { "timestamp": time() }
+    try:
+        producer.produce("items", item.name)
+        return {"timestamp": time()}
+    except KafkaException as ex:
+        raise HTTPException(status_code=500, detail=ex.args[0].str())
+
+
+@app.post("/items5")
+async def create_item5(item: Item):
+    return {"timestamp": time()}
 
 
 if __name__ == '__main__':
