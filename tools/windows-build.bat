284f46a85b9e1f01a58619a9502ac972967f348e
Magnus Edenhill <magnus@edenhill.se>
2020-03-31 12:42:25 +0200

    AppVeyor: upgrade pips

--- tools/windows-build.bat
+++ tools/windows-build.bat
@@ -10,7 +10,7 @@ set
 rem Download and install librdkafka from NuGet.
 call tools\windows-install-librdkafka.bat %LIBRDKAFKA_NUGET_VERSION% dest || exit /b 1
 
-pip install -r tests/requirements.txt
+pip install -U -r tests/requirements.txt
 
 pip install cibuildwheel==0.12.0 || exit /b 1
 
