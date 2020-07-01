93782e858a39f9eaeea57d79f95ac712e373b941
petebids <petebidstrup1988@gmail.com>
2020-07-01 20:38:58 +1000

    second  draft of changes to fix issue #905

--- tests/run.sh
+++ tests/run.sh
@@ -14,7 +14,7 @@ trap cleanup 0 2 3 6 15
 source ${DOCKER_BIN}/../.env
 
 if [[ ${1:-} == "help" ]]; then
-    python3 ${TEST_SOURCE}/integration/integration_test.py --help
+    python ${TEST_SOURCE}/integration/integration_test.py --help
     exit 0
 fi
 
@@ -30,7 +30,7 @@ run_tox() {
 }
 
 run_native() {
-    pip3 install -v .[avro]
+    pip install -v .[avro]
     start_cluster
 
     for mode in "$@"; do
@@ -38,7 +38,7 @@ run_native() {
     done
 
     echo "Executing test modes $@"
-    python3 ${TEST_SOURCE}/integration/integration_test.py ${modes:-} ${TEST_SOURCE}/integration/testconf.json
+    python ${TEST_SOURCE}/integration/integration_test.py ${modes:-} ${TEST_SOURCE}/integration/testconf.json
 }
 
 run_unit() {
