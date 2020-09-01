5203fc83553c9dfc754cd62b9141a06b36d20c84
Magnus Edenhill <magnus@edenhill.se>
2020-09-01 09:30:37 +0200

    Use AK 2.6.0 in system tests

--- tests/system/run-tests.sh
+++ tests/system/run-tests.sh
@@ -24,7 +24,7 @@ export PYTHONUNBUFFERED=1
 TEST_PATH=tests/kafkatest/tests/client
 
 LIBRDKAFKA_BRANCH=master
-KAFKA_BRANCH=2.5
+KAFKA_BRANCH=2.6.0  # Tag
 REPO=https://github.com/apache/kafka.git
 
 CACHE=$WORKSPACE/cache  # Helps with reusing vagrant cluster
