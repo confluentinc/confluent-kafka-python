e6ff95ab117382de640bdbdd59c6112a742bb33c
Magnus Edenhill <magnus@edenhill.se>
2021-01-19 16:44:04 +0100

    Travis: restructure wheel building

---
+++ tools/wheels/build-wheels.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#
+#
+# Build wheels (on Linux or OSX) using cibuildwheel.
+#
+
+this_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+# Skip PyPy, old Python3 versions, and x86 builds.
+export CIBW_SKIP="pp* cp35-* *i686"
+# Run a simple test suite
+export CIBW_TEST_REQUIRES="-r tests/requirements.txt"
+export CIBW_TEST_COMMAND="pytest {project}/tests/test_Producer.py"
+
+
+librdkafka_version=$1
+wheeldir=$2
+
+if [[ -z $wheeldir ]]; then
+    echo "Usage: $0 <librdkafka-nuget-version> <wheeldir>"
+    exit 1
+fi
+
+set -ex
+
+[[ -d $wheeldir ]] || mkdir -p "$wheeldir"
+
+
+case $OSTYPE in
+    linux*)
+        os=linux
+        # Need to set up env vars (in docker) so that setup.py
+        # finds librdkafka.
+        lib_dir=dest/runtimes/linux-x64/native
+        export CIBW_ENVIRONMENT="CFLAGS=-I\$PWD/dest/build/native/include LDFLAGS=-L\$PWD/$lib_dir LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$PWD/$lib_dir"
+        ;;
+    darwin*)
+        os=macos
+        # Need to set up env vars so that setup.py finds librdkafka.
+        lib_dir=dest/runtimes/osx-x64/native
+        export CFLAGS="-I${PWD}/dest/build/native/include"
+        export LDFLAGS="-L${PWD}/$lib_dir"
+        ;;
+    *)
+        echo "$0: Unsupported OSTYPE $OSTYPE"
+        exit 1
+        ;;
+esac
+
+
+$this_dir/install-librdkafka.sh $librdkafka_version dest
+
+install_pkgs=cibuildwheel==1.7.4
+
+python3 -m pip install $install_pkgs ||
+    pip3 install $install_pkgs
+
+if [[ -z $TRAVIS ]]; then
+    cibw_args="--platform $os"
+fi
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/$lib_dir python3 -m cibuildwheel --output-dir $wheeldir $cibw_args
+
+ls $wheeldir
+
+for f in $wheeldir/*whl ; do
+    echo $f
+    unzip -l $f
+done
+
---
+++ tools/wheels/install-librdkafka.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -ex
+
+VER="$1"
+DEST="$2"
+
+if [[ -z $DEST ]]; then
+    echo "Usage: $0 <librdkafka-redist-version> <destdir>"
+    exit 1
+fi
+
+if [[ -f $DEST/build/native/include/librdkafka/rdkafka.h ]]; then
+    echo "$0: librdkafka already installed in $DEST"
+    exit 0
+fi
+
+echo "$0: Installing librdkafka $VER to $DEST"
+[[ -d "$DEST" ]] || mkdir -p "$DEST"
+pushd "$DEST"
+
+curl -L -o lrk$VER.zip https://www.nuget.org/api/v2/package/librdkafka.redist/$VER
+
+unzip lrk$VER.zip
+
+
+if [[ $OSTYPE == linux* ]]; then
+    # Linux
+
+    # Copy the librdkafka build with least dependencies to librdkafka.so.1
+    cp -v runtimes/linux-x64/native/{centos6-librdkafka.so,librdkafka.so.1}
+    ldd runtimes/linux-x64/native/librdkafka.so.1
+
+elif [[ $OSTYPE == darwin* ]]; then
+    # MacOS X
+
+    # Change the library's self-referencing name from
+    # /Users/travis/.....somelocation/librdkafka.1.dylib to its local path.
+    install_name_tool -id $PWD/runtimes/osx-x64/native/librdkafka.dylib runtimes/osx-x64/native/librdkafka.dylib
+
+    otool -L runtimes/osx-x64/native/librdkafka.dylib
+fi
+
+popd
