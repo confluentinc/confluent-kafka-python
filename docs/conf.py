6912831f8268c25dac234bd5c5e77eeb6fd07f1f
Magnus Edenhill <magnus@edenhill.se>
2021-01-27 09:25:20 +0100

    Bump docs copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'confluent-kafka'
-copyright = u'2016-2020, Confluent Inc.'
+copyright = u'2016-2021, Confluent Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
