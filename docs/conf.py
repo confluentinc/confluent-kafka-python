c107497dc7a4f2ec10912d4d0af5933a79b1458f
Magnus Edenhill <magnus@edenhill.se>
2021-02-26 12:20:25 +0100

    Bump to v1.6.1 for Py and librdkafka

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ copyright = u'2016-2021, Confluent Inc.'
 # built documents.
 #
 # The short X.Y version.
-version = '1.6.0'
+version = '1.6.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
