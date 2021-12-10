7f8e024a3c5ec150d050649c1587024fc7fab92c
Jing Liu <jl5311@nyu.edu>
2021-12-09 20:33:17 -0800

    debug message

--- src/confluent_kafka/kafkatest/deploy.sh
+++ src/confluent_kafka/kafkatest/deploy.sh
@@ -62,10 +62,14 @@ if [[ -z $DIR ]]; then
     DIR=/tmp
 fi
 
+echo "Jing test 1"
 [[ -d $DIR ]] || mkdir -p $DIR
+echo "Jing test 2"
 pushd $DIR
+echo "Jing test 3"
 
 mkdir -p $DIR/dist
+echo "Jing test 4"
 
 function setup_virtualenv {
     if [[ ! -f $DIR/venv/bin/activate ]]; then
@@ -84,28 +88,43 @@ function setup_virtualenv {
 
 
 function install_librdkafka {
+    echo "Jing test 5"
     [[ $FORCE_UPDATE == 1 ]] && rm -rf librdkafka
+    echo "Jing test 6"
     mkdir -p librdkafka
+    echo "Jing test 7"
     [[ -f librdkafka/configure ]] || curl -Lq https://github.com/edenhill/librdkafka/archive/master.tar.gz | \
 	    tar -xvf - --strip=1
+    echo "Jing test 8"
     pushd librdkafka
+    echo "Jing test 9"
     ./configure --prefix=$DIR/dist
+    echo "Jing test 10"
     make
+    echo "Jing test 11"
     make install
+    echo "Jing test 12"
     popd
+    echo "Jing test 13"
 }
 
 function install_client {
+    echo "Jing test 14"
     pip uninstall -y confluent_kafka || true
+    echo "Jing test 15"
     pip install -U --only-binary confluent_kafka -f /vagrant/$REL_DIR confluent_kafka
+    echo "Jing test 16"
 }
 
 function verify_client {
+    echo "Jing test 17"
     python -m confluent_kafka.kafkatest.verifiable_consumer --help
+    echo "Jing test 18"
 }
 
 
 if [[ $FORCE_UPDATE != 1 ]]; then
+    echo "Jing test 19"
     verify_client && exit 0
 fi
 
@@ -117,7 +136,9 @@ setup_virtualenv
 if ! verify_client ; then
     echo "Client not installed, installing..."
     install_client
+    echo "Jing test 20"
     verify_client
+    echo "Jing test 21"
 else
     echo "Client already installed"
 fi
