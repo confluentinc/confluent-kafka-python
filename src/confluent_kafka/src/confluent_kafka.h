3010e93dcb2faf18038314ce5d39a17740b60f8d
Magnus Edenhill <magnus@edenhill.se>
2021-12-13 09:58:49 +0100

    Bump version to v1.8.2 to align with embedded librdkafka version

--- src/confluent_kafka/src/confluent_kafka.h
+++ src/confluent_kafka/src/confluent_kafka.h
@@ -42,8 +42,8 @@
  *  0xMMmmRRPP
  *  MM=major, mm=minor, RR=revision, PP=patchlevel (not used)
  */
-#define CFL_VERSION     0x01080000
-#define CFL_VERSION_STR "1.8.0"
+#define CFL_VERSION     0x01080200
+#define CFL_VERSION_STR "1.8.2"
 
 /**
  * Minimum required librdkafka version. This is checked both during
