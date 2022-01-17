c743e4aaf5fe1dc84b6d53214d86d931e914a617
Magnus Edenhill <magnus@edenhill.se>
2022-01-17 12:32:38 +0100

    bootstrap-librdkafka: support cached builddir

--- tools/bootstrap-librdkafka.sh
+++ tools/bootstrap-librdkafka.sh
@@ -3,6 +3,7 @@
 #
 # Downloads, builds and installs librdkafka into <install-dir>
 #
+# An existing build-dir with the same version can be reused.
 
 if [[ $1 == "--require-ssl" ]]; then
     REQUIRE_SSL=1
@@ -11,42 +12,65 @@ fi
 
 VERSION=$1
 INSTALLDIR=$2
-BUILDDIR=$PWD/tmp-build
+BUILDDIR="$3"
+
+if [ -z $BUILDDIR ]; then
+    BUILDDIR=$PWD/tmp-build
+fi
+
+if [[ -z "$VERSION" ]]; then
+    echo "Usage: $0 [--require-ssl] <librdkafka-version> [<install-dir> [<build-dir>]]" 1>&2
+    exit 1
+fi
 
 set -ex
 set -o pipefail
-
-if [[ -z "$VERSION" ]]; then
-    echo "Usage: $0 --require-ssl <librdkafka-version> [<install-dir>]" 1>&2
-    exit 1
-fi
 
 if [[ $INSTALLDIR != /* ]]; then
     INSTALLDIR="$PWD/$INSTALLDIR"
 fi
 
-mkdir -p "$BUILDDIR/librdkafka"
-pushd "$BUILDDIR/librdkafka"
+if [[ -f $BUILDDIR/.stamp ]]; then
+    prevver=$(cat $BUILDDIR/.stamp)
+    if [[ $prevver != $VERSION ]]; then
+        echo "$0: ERROR: Existing build directory $BUILDDIR contains $prevver, not $VERSION"
+        exit 1
+    fi
+    echo "$0: Reusing build directory $BUILDDIR for version $VERSION"
+    pushd "$BUILDDIR/librdkafka"
 
-test -f configure ||
-curl -q -L "https://github.com/edenhill/librdkafka/archive/${VERSION}.tar.gz" | \
-    tar -xz --strip-components=1 -f -
+    if [[ $REQUIRE_SSL == 1 ]]; then
+        grep '^#define WITH_SSL 1$' config.h || \
+            (echo "ERROR: OpenSSL support required" ; cat config.log config.h ; exit 1)
+    fi
 
-./configure --clean
-make clean
+else
 
-if [[ $OSTYPE == "linux"* ]]; then
-  EXTRA_OPTS="--disable-gssapi"
+    mkdir -p "$BUILDDIR/librdkafka"
+    pushd "$BUILDDIR/librdkafka"
+
+    test -f configure ||
+        curl -q -L "https://github.com/edenhill/librdkafka/archive/${VERSION}.tar.gz" | \
+            tar -xz --strip-components=1 -f -
+
+    ./configure --clean
+    make clean
+
+    if [[ $OSTYPE == "linux"* ]]; then
+        EXTRA_OPTS="--disable-gssapi"
+    fi
+
+    ./configure --enable-static --install-deps --source-deps-only $EXTRA_OPTS --prefix="$INSTALLDIR"
+
+    if [[ $REQUIRE_SSL == 1 ]]; then
+        grep '^#define WITH_SSL 1$' config.h || \
+            (echo "ERROR: OpenSSL support required" ; cat config.log config.h ; exit 1)
+    fi
+
+    make -j
 fi
 
-./configure --enable-static --install-deps --source-deps-only $EXTRA_OPTS --prefix="$INSTALLDIR"
 
-if [[ $REQUIRE_SSL == 1 ]]; then
-    grep '^#define WITH_SSL 1$' config.h || \
-        (echo "ERROR: OpenSSL support required" ; cat config.log config.h ; exit 1)
-fi
-
-make -j
 examples/rdkafka_example -X builtin.features
 
 if [[ $INSTALLDIR == /usr && $(whoami) != root ]]; then
@@ -54,5 +78,7 @@ if [[ $INSTALLDIR == /usr && $(whoami) != root ]]; then
 else
     make install
 fi
+
 popd
+echo "$VERSION" > $BUILDDIR/.stamp
 
