742ff4ff952948a7250a0ac68fbfd37c5981d6c2
Jing Liu <jl5311@nyu.edu>
2022-01-24 14:41:26 -0800

    Update client path

--- tests/system/run-tests.sh
+++ tests/system/run-tests.sh
@@ -21,7 +21,7 @@ set -x
 # Immediately flush output when running python
 export PYTHONUNBUFFERED=1
 
-TEST_PATH=tests/kafkatest/tests/client/consumer_test.py
+TEST_PATH=tests/kafkatest/tests/client
 
 LIBRDKAFKA_BRANCH=v1.8.2
 KAFKA_BRANCH=3.0.0  # Tag
