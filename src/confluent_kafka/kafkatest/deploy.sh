f57b4e7466650f4265ea694c18b97d02d8274e21
Jing Liu <jl5311@nyu.edu>
2022-02-07 22:24:42 -0800

    python 3.8

--- src/confluent_kafka/kafkatest/deploy.sh
+++ src/confluent_kafka/kafkatest/deploy.sh
@@ -112,9 +112,9 @@ function install_librdkafka {
 }
 
 function install_client {
-    sudo apt update && sudo apt install -y sudo python3-pip libpq-dev python3-dev libffi-dev libssl-dev && sudo sapt-get -y clean
+    sudo apt update && sudo apt install -y sudo python3-pip libpq-dev python3-dev libffi-dev libssl-dev python3.8 && sudo apt-get -y clean
     echo "pip upgrade"
-    sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+    sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 10
     sudo pip install --upgrade pip
     #sudo python3 -m pip install --upgrade pip
     echo "Jing test 14"
