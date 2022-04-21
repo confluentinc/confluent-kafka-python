bd6862d02103f407135905e6f399b53401213472
Jing Liu <jl5311@nyu.edu>
2022-04-21 11:08:42 -0700

    truncation_test

--- tests/system/run-tests.sh
+++ tests/system/run-tests.sh
@@ -25,7 +25,7 @@ set -x
 # Immediately flush output when running python
 export PYTHONUNBUFFERED=1
 
-TEST_PATH=tests/kafkatest/tests/client/consumer_rolling_upgrade_test.py
+TEST_PATH=tests/kafkatest/tests/client/truncation_test.py
 
 LIBRDKAFKA_BRANCH=master
 KAFKA_BRANCH=forSystemTest  # Tag
