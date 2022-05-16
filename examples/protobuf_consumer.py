6d0a69e1d673947139148268081915cce5620685
Kevan Swanberg <kevswanberg@gmail.com>
2022-05-16 17:53:18 -0400

    Format args in the correct order

--- examples/protobuf_consumer.py
+++ examples/protobuf_consumer.py
@@ -68,8 +68,8 @@ def main(args):
                       "\tfavorite_number: {}\n"
                       "\tfavorite_color: {}\n"
                       .format(msg.key(), user.name,
-                              user.favorite_color,
-                              user.favorite_number))
+                              user.favorite_number,
+                              user.favorite_color))
         except KeyboardInterrupt:
             break
 
