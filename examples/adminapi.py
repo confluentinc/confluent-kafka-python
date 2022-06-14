0f702ce74ca5d53bfda557773f23fe35a3a6d7a4
Emanuele Sabellico <esabellico@confluent.io>
2022-06-14 21:02:40 +0200

    removed None check

--- examples/adminapi.py
+++ examples/adminapi.py
@@ -159,10 +159,6 @@ def example_create_acls(a, args):
         print(f"Invalid input: {e}")
         return
 
-    if fs is None:
-        print("Unknown error: fs is None")
-        return
-
     # Wait for operation to finish.
     for res, f in fs.items():
         try:
@@ -251,10 +247,6 @@ def example_delete_acls(a, args):
         fs = a.delete_acls(acl_binding_filters, request_timeout=10)
     except ValueError as e:
         print(f"Invalid input: {e}")
-        return
-    
-    if fs is None:
-        print("Unknown error: fs is None")
         return
 
     # Wait for operation to finish.
