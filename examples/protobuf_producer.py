5936d0b465ad45eaff9c02ed7829f02d7c797cd7
Matt Howlett <matt@confluent.io>
2022-09-14 16:45:19 -0500

    Serialization Stabilization (#1426)
    * tidy up json serde (#1340)
    
    * tidy up json serde
    
    * to_dict doc tweaks
    
    * serializer text tweak
    
    * produces -> outputs
    
    * address review comments
    
    * minor tweak
    
    * tidy up protobuf serde (#1337)
    
    * tidy up protobuf serde
    
    * tweak sr explanation text
    
    * tweak SerializeError text
    
    * changes following review of JSON Schema PR
    
    * Fixed failing test cases by changing the function names (#1419)
    
    * tidy up avro serde + additional polish (#1413)
    
    * tidy up avro serde
    
    * SerializingProducer & DeserializingConsumer polish + other tweaks
    
    * Deemphasise SerializingProducer and DeserializingConsumer
    
    * Removing use of SerializingProducer and DeserializingConsumer
    
    * Tweak examples readme
    
    * CI error fixes
    
    * Additional CI error fixes
    
    * Additional CI error fix
    
    * Fix examples
    
    * fix flake8 error
    
    * A bit of README.md polish (#1424)
    
    * A bit of README.md polish
    
    * additional tweaks
    
    * updates based on review feedback
    
    Co-authored-by: Pranav Rathi <pranav.rth@gmail.com>

--- examples/protobuf_producer.py
+++ examples/protobuf_producer.py
@@ -14,12 +14,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
+
+# A simple example demonstrating use of ProtobufSerializer.
 #
-# This is a simple example of the SerializingProducer using protobuf.
-#
-# To regenerate Protobuf classes you must first install the protobuf
+# To create Protobuf classes you must first install the protobuf
 # compiler. Once installed you may call protoc directly or use make.
 #
 # See the protocol buffer docs for instructions on installing and using protoc.
@@ -28,7 +27,7 @@
 # After installing protoc execute the following command from the examples
 # directory to regenerate the user_pb2 module.
 # `make`
-#
+
 import argparse
 from uuid import uuid4
 
@@ -36,8 +35,8 @@ from six.moves import input
 
 # Protobuf generated class; resides at ./protobuf/user_pb2.py
 import protobuf.user_pb2 as user_pb2
-from confluent_kafka import SerializingProducer
-from confluent_kafka.serialization import StringSerializer
+from confluent_kafka import Producer
+from confluent_kafka.serialization import StringSerializer, SerializationContext, MessageField
 from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.protobuf import ProtobufSerializer
 
@@ -48,18 +47,9 @@ def delivery_report(err, msg):
 
     Args:
         err (KafkaError): The error that occurred on None on success.
-
         msg (Message): The message that was produced or failed.
-
-    Note:
-        In the delivery report callback the Message.key() and Message.value()
-        will be the binary format as encoded by any configured Serializers and
-        not the same object that was passed to produce().
-        If you wish to pass the original object(s) for key and value to delivery
-        report callback we recommend a bound callback or lambda where you pass
-        the objects along.
-
     """
+
     if err is not None:
         print("Delivery failed for User record {}: {}".format(msg.key(), err))
         return
@@ -73,15 +63,14 @@ def main(args):
     schema_registry_conf = {'url': args.schema_registry}
     schema_registry_client = SchemaRegistryClient(schema_registry_conf)
 
+    string_serializer = StringSerializer('utf8')
     protobuf_serializer = ProtobufSerializer(user_pb2.User,
                                              schema_registry_client,
                                              {'use.deprecated.format': False})
 
-    producer_conf = {'bootstrap.servers': args.bootstrap_servers,
-                     'key.serializer': StringSerializer('utf_8'),
-                     'value.serializer': protobuf_serializer}
+    producer_conf = {'bootstrap.servers': args.bootstrap_servers}
 
-    producer = SerializingProducer(producer_conf)
+    producer = Producer(producer_conf)
 
     print("Producing user records to topic {}. ^C to exit.".format(topic))
     while True:
@@ -94,7 +83,9 @@ def main(args):
             user = user_pb2.User(name=user_name,
                                  favorite_color=user_favorite_color,
                                  favorite_number=user_favorite_number)
-            producer.produce(topic=topic, partition=0, key=str(uuid4()), value=user,
+            producer.produce(topic=topic, partition=0,
+                             key=string_serializer(str(uuid4())),
+                             value=protobuf_serializer(user, SerializationContext(topic, MessageField.VALUE)),
                              on_delivery=delivery_report)
         except (KeyboardInterrupt, EOFError):
             break
@@ -107,7 +98,7 @@ def main(args):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="SerializingProducer Example")
+    parser = argparse.ArgumentParser(description="ProtobufSerializer example")
     parser.add_argument('-b', dest="bootstrap_servers", required=True,
                         help="Bootstrap broker(s) (host[:port])")
     parser.add_argument('-s', dest="schema_registry", required=True,
