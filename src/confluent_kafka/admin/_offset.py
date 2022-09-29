6d365adaab10a09c5c5a2b119d868cb30dcdbf13
Pranav Rathi <pranav.rth@gmail.com>
2022-09-29 05:43:00 +0530

    Changed import from ..cimpl instead of confluent_kafka for _offsets.py file

--- src/confluent_kafka/admin/_offset.py
+++ src/confluent_kafka/admin/_offset.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from confluent_kafka import TopicPartition, OFFSET_INVALID
+from ..cimpl import TopicPartition, OFFSET_INVALID
 from abc import ABC, abstractmethod
 
 try:
