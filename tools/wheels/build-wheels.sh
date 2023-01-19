fb49706729ac58496f10325209d2eee7d3f1d268
Pranav Rathi <pranav.rth@gmail.com>
2023-01-19 06:13:43 +0530

    Installing python before running cibuildwheel

--- tools/wheels/build-wheels.sh
+++ tools/wheels/build-wheels.sh
@@ -8,7 +8,7 @@ this_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 
 # Skip PyPy, Python2, old Python3 versions, musl, and x86 builds.
-export CIBW_SKIP="pp* cp27-* cp35-* cp36-* *i686 *musllinux* $CIBW_SKIP"
+export CIBW_SKIP="pp* cp27-* cp35-* *i686 *musllinux* $CIBW_SKIP"
 # Run a simple test suite
 export CIBW_TEST_REQUIRES="-r tests/requirements.txt"
 export CIBW_TEST_COMMAND="pytest {project}/tests/test_Producer.py"
@@ -16,6 +16,7 @@ export CIBW_TEST_COMMAND="pytest {project}/tests/test_Producer.py"
 
 librdkafka_version=$1
 wheeldir=$2
+cibuildwheel_version="2.12.0"
 
 if [[ -z $wheeldir ]]; then
     echo "Usage: $0 <librdkafka-nuget-version> <wheeldir>"
@@ -52,13 +53,17 @@ esac
 
 $this_dir/install-librdkafka.sh $librdkafka_version dest
 
-install_pkgs=cibuildwheel==2.12.0
+install_pkgs=cibuildwheel==$cibuildwheel_version
 
 python3 -m pip install ${PIP_INSTALL_OPTS} $install_pkgs ||
     pip3 install ${PIP_INSTALL_OPTS} $install_pkgs
 
 if [[ -z $TRAVIS ]]; then
     cibw_args="--platform $os"
+fi
+
+if [[ os == "macos"]]; then
+    python3 $this_dir/install-macos-required-python.py $cibuildwheel_version
 fi
 
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/$lib_dir python3 -m cibuildwheel --output-dir $wheeldir $cibw_args
