6ad7de7f15b2729eb4442ab047c9ebe0d1cec277
PrasanthV454 <vpk11@iitbbs.ac.in>
2023-01-23 03:04:43 +0530

    comamnds updated

--- tools/mingw-w64/semaphore_commands.sh
+++ tools/mingw-w64/semaphore_commands.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -e
+pacman -S python --version 3.8.0
+export PATH="/c/Python38:/c/Python38/Scripts:$PATH"
+ln -s /c/Python38/python.exe /c/Python38/python3.exe
 export MAKE=mingw32-make  # so that Autotools can find it
