c15b1a016ead6bf2c8dba0e6d271428aa0239f18
PrasanthV454 <vpk11@iitbbs.ac.in>
2023-01-23 18:05:51 +0530

    comamnds updated

--- tools/mingw-w64/semaphore_commands.sh
+++ tools/mingw-w64/semaphore_commands.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-set -e
 pacman -S python --version 3.8.0
+
+set -e
+
 export PATH="$PATH;C:\Python38;C:\Python38\Scripts"
-cmd /c mklink /D C:\Python38\python3.exe C:\Python38\python.exe
 export MAKE=mingw32-make  # so that Autotools can find it
+export LIBRDKAFKA_VERSION=v2.0.2
+
+cmd /c mklink /D C:\Python38\python3.exe C:\Python38\python.exe
