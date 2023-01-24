be882c3ca81a6344d3e5e6383cb0b35bd3c9e7e7
PrasanthV454 <vpk11@iitbbs.ac.in>
2023-01-24 14:07:56 +0530

    comamnds updated

--- tools/wheels/build-wheels.bat
+++ tools/wheels/build-wheels.bat
@@ -25,11 +25,7 @@ set CIBW_REPAIR_WHEEL_COMMAND=python -m delvewheel repair --add-path %DLL_DIR% -
 
 set PATH=%PATH%;c:\Program Files\Git\bin\
 
-python -m pip install cibuildwheel==2.12.0
-
-mkdir %WHEELHOUSE%
-
-python -m cibuildwheel --output-dir %WHEELHOUSE% --platform windows
+python -m cibuildwheel --output-dir %WHEELHOUSE% --platform windows || goto :error
 
 dir %WHEELHOUSE%
 
