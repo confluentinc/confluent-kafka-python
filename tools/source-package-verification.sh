ba063ff0c0fc87afc661cb43e1dce2eef78519ce
PrasanthV454 <vpk11@iitbbs.ac.in>
2023-02-08 17:09:31 +0530

    changes

--- tools/source-package-verification.sh
+++ tools/source-package-verification.sh
@@ -8,19 +8,12 @@ pip install -r docs/requirements.txt
 pip install -U protobuf
 pip install -r tests/requirements.txt
 
+lib_dir=dest/runtimes/$OS_NAME-$ARCH/native
 tools/wheels/install-librdkafka.sh "${LIBRDKAFKA_VERSION#v}" dest
-if [[ $OS_NAME == windows ]]; then
-    export include="$include -I${PWD}\dest\build\native\include"
-    export lib="$lib -L${PWD}\dest\build\native\lib\win\%ARCH%\win-${ARCH}-Release\v142"
-    export DLL_DIR="$DLL_DIR -L${PWD}\dest\runtimes\win-${ARCH}\native"
-    tools/windows-install-librdkafka.bat "${LIBRDKAFKA_VERSION#v}" dest
-else
-    lib_dir=dest/runtimes/$OS_NAME-$ARCH/native
-    export CFLAGS="$CFLAGS -I${PWD}/dest/build/native/include"
-    export LDFLAGS="$LDFLAGS -L${PWD}/${lib_dir}"
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PWD/$lib_dir"
-    export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$PWD/$lib_dir"
-fi
+export CFLAGS="$CFLAGS -I${PWD}/dest/build/native/include"
+export LDFLAGS="$LDFLAGS -L${PWD}/${lib_dir}"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PWD/$lib_dir"
+export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$PWD/$lib_dir"
 
 python setup.py build && python setup.py install
 if [[ $OS_NAME == linux && $ARCH == x64 ]]; then
