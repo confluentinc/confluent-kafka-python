727c5e619219300e4f402cc007c5d6eaa34a5c4a
PrasanthV454 <vpk11@iitbbs.ac.in>
2023-02-16 10:34:06 +0530

    changes

--- tools/source-package-verification.sh
+++ tools/source-package-verification.sh
@@ -17,7 +17,7 @@ export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$PWD/$lib_dir"
 
 python setup.py build && python setup.py install
 if [[ $OS_NAME == linux && $ARCH == x64 ]]; then
-    flake8
+    flake8 --exclude ./_venv
     make docs
     python -m pytest --timeout 600 --ignore=dest
 else
