0aa708b0d679144071f32efda8c6e04e7de86d80
Pranav Rathi <4427674+pranavrth@users.noreply.github.com>
2023-09-11 01:45:21 +0530

    Added more generalization

--- tests/integration/admin/test_authorized_operations.py
+++ tests/integration/admin/test_authorized_operations.py
@@ -28,7 +28,6 @@ from confluent_kafka.error import ConsumeError, KafkaException, KafkaError
 
 topic_prefix = "test-topic"
 
-
 # Shared between producer and consumer tests and used to verify
 # that consumed headers are what was actually produced.
 produce_headers = [('foo1', 'bar'),
@@ -40,19 +39,18 @@ produce_headers = [('foo1', 'bar'),
                    ('foobin', struct.pack('hhl', 10, 20, 30))]
 
 
-def verify_commit_result(err, partitions):
+def verify_commit_result(err, _):
     assert err is not None
+
 
 def consume_messages(sasl_cluster, group_id, topic, num_messages=None):
     conf = {'group.id': group_id,
             'session.timeout.ms': 6000,
             'enable.auto.commit': False,
             'on_commit': verify_commit_result,
-            'auto.offset.reset': 'earliest',
-            'enable.partition.eof': True}
+            'auto.offset.reset': 'earliest'}
     consumer = sasl_cluster.consumer(conf)
     consumer.subscribe([topic])
-    eof_reached = dict()
     read_messages = 0
     msg = None
     while True:
@@ -67,30 +65,63 @@ def consume_messages(sasl_cluster, group_id, topic, num_messages=None):
                 print('Read all the required messages: exiting')
                 break
         except ConsumeError as e:
-            if msg is not None and e.code == confluent_kafka.KafkaError._PARTITION_EOF:
-                print('Reached end of %s [%d] at offset %d' % (
-                        msg.topic(), msg.partition(), msg.offset()))
-                eof_reached[(msg.topic(), msg.partition())] = True
-                if len(eof_reached) == len(consumer.assignment()):
-                    print('EOF reached for all assigned partitions: exiting')
-                    break
-            else:
-                print('Consumer error: %s: ignoring' % str(e))
-                break
+            print('Consumer error: %s: ignoring' % str(e))
+            break
+        except Exception:
+            raise
     consumer.close()
 
+
+def perform_admin_operation_sync(operation, *arg, **kwargs):
+    future_key = arg[0][0] if len(arg) > 0 else None
+    fs = operation(*arg, **kwargs)
+    fs = fs[future_key] if future_key else fs 
+    return fs.result()
+
+
 def create_acls(admin_client, acl_bindings):
-    fs = admin_client.create_acls(acl_bindings)
-    for acl_binding, f in fs.items():
-        f.result()  # trigger exception if there was an error
+    perform_admin_operation_sync(admin_client.create_acls, acl_bindings)
+
 
 def delete_acls(admin_client, acl_binding_filters):
-    fs = admin_client.delete_acls(acl_binding_filters)
-    for acl_binding_filters, f in fs.items():
-        f.result()  # trigger exception if there was an error
+    perform_admin_operation_sync(admin_client.delete_acls, acl_binding_filters)
+
+
+def verify_provided_describe_for_authorized_operations(admin_client, describe_fn, operation_to_allow, operation_to_check, restype, resname, *arg):
+    kwargs = {}
+    kwargs['request_timeout'] = 10
+
+    # Check with include_authorized_operations as False
+    kwargs['include_authorized_operations'] = False
+    desc = perform_admin_operation_sync(describe_fn, *arg, **kwargs)
+    assert desc.authorized_operations == []
+
+    # Check with include_authorized_operations as True
+    kwargs['include_authorized_operations'] = True
+    desc = perform_admin_operation_sync(describe_fn, *arg, **kwargs)
+    assert len(desc.authorized_operations) > 0
+    assert operation_to_check in desc.authorized_operations
+
+    # Update Authorized Operation by creating new ACLs
+    acl_binding = AclBinding(restype, resname, ResourcePatternType.LITERAL,
+                               "User:sasl_user", "*", operation_to_allow, AclPermissionType.ALLOW)
+    create_acls(admin_client, [acl_binding])
+    time.sleep(2)
+
+    # Check with updated authorized operations
+    desc = perform_admin_operation_sync(describe_fn, *arg, **kwargs)
+    assert len(desc.authorized_operations) > 0
+    assert operation_to_check not in desc.authorized_operations
+
+    # Delete Updated ACLs
+    acl_binding_filter = AclBindingFilter(restype, resname, ResourcePatternType.ANY,
+                                           None, None, AclOperation.ANY, AclPermissionType.ANY)
+    delete_acls(admin_client, [acl_binding_filter])
+    time.sleep(2)
+
 
 def verify_describe_topics(admin_client, topic):
-    verify_describe_generic(admin_client, 
+    verify_provided_describe_for_authorized_operations(admin_client, 
                             admin_client.describe_topics, 
                             AclOperation.READ, 
                             AclOperation.DELETE, 
@@ -99,32 +130,33 @@ def verify_describe_topics(admin_client, topic):
                             [topic])
 
 
-def verify_describe_group(sasl_cluster, admin_client, our_topic):
+def verify_describe_group(cluster, admin_client, our_topic):
 
     # Produce some messages
-    p = sasl_cluster.producer()
+    p = cluster.producer()
     p.produce(our_topic, 'Hello Python!', headers=produce_headers)
     p.produce(our_topic, key='Just a key and headers', headers=produce_headers)
     p.flush()
 
     # Consume some messages for the group
     group = 'test-group'
-    consume_messages(sasl_cluster, group, our_topic, 2)
+    consume_messages(cluster, group, our_topic, 2)
 
-    verify_describe_generic(admin_client,
+    # Verify Describe Consumer Groups
+    verify_provided_describe_for_authorized_operations(admin_client,
                             admin_client.describe_consumer_groups,
                             AclOperation.READ,
                             AclOperation.DELETE,
-                            ResourceType.GROUP, group,
+                            ResourceType.GROUP, 
+                            group,
                             [group])
 
-    # Delete groups
-    fs = admin_client.delete_consumer_groups([group], request_timeout=10)
-    fs[group].result()  # will raise exception on failure
+    # Delete group
+    perform_admin_operation_sync(admin_client.delete_consumer_groups, [group], request_timeout=10)
 
 
 def verify_describe_cluster(admin_client):
-    verify_describe_generic(admin_client, 
+    verify_provided_describe_for_authorized_operations(admin_client, 
                             admin_client.describe_cluster, 
                             AclOperation.ALTER, 
                             AclOperation.ALTER_CONFIGS, 
@@ -132,72 +164,29 @@ def verify_describe_cluster(admin_client):
                             "kafka-cluster")
 
 
-def verify_describe_generic(admin_client, describe, operation_to_allow, operation_to_check, restype, resname, *arg):
-
-    future_key = arg[0][0] if len(arg) > 0 else None
-    kwargs = {}
-    kwargs['request_timeout'] = 10
-
-    # Check with include_authorized_operations as False
-    kwargs['include_authorized_operations'] = False
-    fs = describe(*arg, **kwargs)
-    fs = fs[future_key] if future_key else fs 
-    try:
-        desc = fs.result()
-        assert desc.authorized_operations == []
-    except Exception:
-        raise
-
-    # Check with include_authorized_operations as True
-    kwargs['include_authorized_operations'] = True
-    fs = describe(*arg, **kwargs)
-    fs = fs[future_key] if future_key else fs 
-    try:
-        desc = fs.result()
-        assert len(desc.authorized_operations) > 0
-        assert operation_to_check in desc.authorized_operations
-    except Exception:
-        raise
-
-    # Update Authorized Operation by creating new ACLs
-    acl_binding = AclBinding(restype, resname, ResourcePatternType.LITERAL,
-                               "User:*", "*", operation_to_allow, AclPermissionType.ALLOW)
-    create_acls(admin_client, [acl_binding])
-    time.sleep(2)
-
-    # Check with updated authorized operations
-    fs = describe(*arg, **kwargs)
-    fs = fs[future_key] if future_key else fs 
-    try:
-        desc = fs.result()
-        assert len(desc.authorized_operations) > 0
-        assert operation_to_check not in desc.authorized_operations
-    except Exception:
-        raise
-
-    # Delete Updated ACLs
-    acl_binding_filter = AclBindingFilter(restype, resname, ResourcePatternType.ANY,
-                                           None, None, AclOperation.ANY, AclPermissionType.ANY)
-    delete_acls(admin_client, [acl_binding_filter])
-    time.sleep(2)
-
 def test_authorized_operations(sasl_cluster):
-    num_partitions = 1
-    topic_config = {"compression.type": "gzip"}
 
+    admin_client = sasl_cluster.admin()
+
+    # Verify Authorized Operations in Describe Cluster
+    verify_describe_cluster(admin_client)
+
+    # Create Topic
+    topic_config = {"compression.type": "gzip"}
     our_topic = sasl_cluster.create_topic(topic_prefix,
                                         {
-                                            "num_partitions": num_partitions,
+                                            "num_partitions": 1,
                                             "config": topic_config,
                                             "replication_factor": 1,
                                         },
                                         validate_only=False
                                         )
 
-    admin_client = sasl_cluster.admin()
-
-    # Describe Topic with Authorized Operations
+    # Verify Authorized Operations in Describe Topics
     verify_describe_topics(admin_client, our_topic)
-    verify_describe_group(sasl_cluster, admin_client, our_topic)
-    verify_describe_cluster(admin_client)
 
+    # Verify Authorized Operations in Describe Groups
+    verify_describe_group(sasl_cluster, admin_client, our_topic)
+
+    # Delete Topic
+    perform_admin_operation_sync(admin_client.delete_topics, [our_topic], operation_timeout=0, request_timeout=10)
