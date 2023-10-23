a945782b82e7ee9ff17bbf5134eeb17023219252
Pranav Rathi <4427674+pranavrth@users.noreply.github.com>
2023-10-23 15:38:29 +0530

    Added topic id to describe group response (#1645)
    Added topic id to describe group response

--- src/confluent_kafka/admin/_topic.py
+++ src/confluent_kafka/admin/_topic.py
@@ -26,14 +26,19 @@ class TopicDescription:
     ----------
     name : str
         The topic name.
+    topic_id: Uuid
+        The topic id of the topic
+    is_internal:
+        Whether the topic is internal or not
     partitions : list(TopicPartitionInfo)
         Partition information.
     authorized_operations: list(AclOperation)
         AclOperations allowed for the topic.
     """
 
-    def __init__(self, name, is_internal, partitions, authorized_operations=None):
+    def __init__(self, name, topic_id, is_internal, partitions, authorized_operations=None):
         self.name = name
+        self.topic_id = topic_id
         self.is_internal = is_internal
         self.partitions = partitions
         self.authorized_operations = None
