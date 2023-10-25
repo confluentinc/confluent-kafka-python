462ff051f469716f8d874a7a998d9db5de0f4683
Emanuele Sabellico <esabellico@confluent.io>
2023-10-25 13:11:13 +0200

    Fix test_docs

--- src/confluent_kafka/_model/__init__.py
+++ src/confluent_kafka/_model/__init__.py
@@ -137,12 +137,13 @@ class TopicPartitionInfo:
 class IsolationLevel(Enum):
     """
     Enum for Kafka isolation levels.
+
+    Values:
+    -------
     """
 
-    #: Skip offsets belonging to an aborted transaction.
-    READ_COMMITTED = cimpl.ISOLATION_LEVEL_READ_COMMITTED
-    #: Receive all the offsets.
-    READ_UNCOMMITTED = cimpl.ISOLATION_LEVEL_READ_UNCOMMITTED
+    READ_UNCOMMITTED = cimpl.ISOLATION_LEVEL_READ_UNCOMMITTED #: Receive all the offsets.
+    READ_COMMITTED = cimpl.ISOLATION_LEVEL_READ_COMMITTED #: Skip offsets belonging to an aborted transaction.
 
     def __lt__(self, other):
         if self.__class__ != other.__class__:
