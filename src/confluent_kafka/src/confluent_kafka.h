0be479b5234f0cc867e59aad19a570484eca3d1c
Pranav Rathi <4427674+pranavrth@users.noreply.github.com>
2024-05-07 20:06:11 +0530

    Version upgrade to 2.4.0

--- src/confluent_kafka/src/confluent_kafka.h
+++ src/confluent_kafka/src/confluent_kafka.h
@@ -43,7 +43,7 @@
  *  MM=major, mm=minor, RR=revision, PP=patchlevel (not used)
  */
 #define CFL_VERSION     0x02040000
-#define CFL_VERSION_STR "2.4.0rc3"
+#define CFL_VERSION_STR "2.4.0"
 
 /**
  * Minimum required librdkafka version. This is checked both during
