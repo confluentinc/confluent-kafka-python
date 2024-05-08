ecaa6fe354a2902e4ae9b66c667d867c9d43247f
PratRanj07 <pranjan@confluent.io>
2024-05-08 16:33:37 +0530

    Revert "Merge branch 'deleterecords' into electLeaders"
    This reverts commit f847a7a73b8e6dc3fcc45670ecc647ca48355bfc, reversing
    changes made to 893fc800bee1f853a926d039d6d742ce2236bbea.

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ copyright = u'2016-2023, Confluent Inc.'
 # built documents.
 #
 # The short X.Y version.
-version = '2.4.0'
+version = '2.3.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 ######################################################################
