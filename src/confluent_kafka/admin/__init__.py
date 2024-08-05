1be65c5621d73bf45d3b4005c1716f733db8a20f
John "Preston" Mille <john@ews-network.net>
2024-08-05 12:12:59 +0100

    Adding int ID for Transactional ID Resource Type

--- src/confluent_kafka/admin/__init__.py
+++ src/confluent_kafka/admin/__init__.py
@@ -75,6 +75,7 @@ from ..cimpl import (KafkaException,  # noqa: F401
                      RESOURCE_TOPIC,
                      RESOURCE_GROUP,
                      RESOURCE_BROKER,
+                     RESOURCE_TRANSACTION,
                      OFFSET_INVALID)
 
 from confluent_kafka import \
