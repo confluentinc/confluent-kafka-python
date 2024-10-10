f354a7a23fa9d38f450d279781a3c0a6e5956230
Pranav Rathi <4427674+pranavrth@users.noreply.github.com>
2024-10-11 03:22:33 +0530

    Changes for v2.6.0 release (#1836)

--- src/confluent_kafka/src/confluent_kafka.h
+++ src/confluent_kafka/src/confluent_kafka.h
@@ -43,7 +43,7 @@
  *  MM=major, mm=minor, RR=revision, PP=patchlevel (not used)
  */
 #define CFL_VERSION     0x02060000
-#define CFL_VERSION_STR "2.6.0rc2"
+#define CFL_VERSION_STR "2.6.0"
 
 /**
  * Minimum required librdkafka version. This is checked both during
