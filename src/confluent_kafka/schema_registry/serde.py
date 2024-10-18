5bcbf0829c7e77c44d4c3e477d13a4d3182c436c
Robert Yokota <rayokota@gmail.com>
2024-10-17 18:31:55 -0700

    Checkpoint

--- src/confluent_kafka/schema_registry/serde.py
+++ src/confluent_kafka/schema_registry/serde.py
@@ -17,17 +17,22 @@
 #
 
 __all__ = ['BaseSerializer',
-           'BaseDeserializer']
+           'BaseDeserializer',
+           'RuleAction',
+           'RuleExecutor']
 
+import logging
 from enum import IntEnum
 from typing import Callable, List, Optional
 
 from confluent_kafka.schema_registry import RegisteredSchema
 from confluent_kafka.schema_registry.schema_registry_client import RuleMode, \
-    Rule
+    Rule, RuleKind
 from confluent_kafka.serialization import Serializer, Deserializer, \
-    SerializationContext
+    SerializationContext, SerializationError
 
+
+log = logging.getLogger(__name__)
 
 class RuleContext(object):
     __slots__ = ['ctx', 'subject', 'rule_mode', 'source', 'target', 'message', 'field_transformer']
@@ -43,7 +48,15 @@ class RuleContext(object):
 
 
 class RuleBase(object):
-    pass
+
+    def configure(self, client_conf, conf):
+        pass
+
+    def type(self) -> str:
+        raise NotImplementedError
+
+    def close(self):
+        pass
 
 
 class RuleExecutor(RuleBase):
@@ -119,8 +132,9 @@ class BaseSerde(object):
             latest_schema = self._registry.get_latest_version(subject)
         return latest_schema
 
-    def _execute_rules(self, ctx: SerializationContext, subject: str,
-        rule_mode: RuleMode, source: RegisteredSchema, target: RegisteredSchema,
+    def _execute_rules(self, ser_ctx: SerializationContext, subject: str,
+        rule_mode: RuleMode,
+        source: Optional[RegisteredSchema], target: Optional[RegisteredSchema],
         message: object, field_transformer: FieldTransformer) -> object:
         if message is None or target is None:
             return None
@@ -150,13 +164,64 @@ class BaseSerde(object):
             elif rule.mode != rule_mode:
                 continue
 
-            ctx = RuleContext(ctx, subject, rule_mode, source, target, message, field_transformer)
-            #rule_executor = getRuleExecutor(self._rule_registry, rule.type.upper())
-
-
-
-        # TODO
+            ctx = RuleContext(ser_ctx, subject, rule_mode, source, target, message, field_transformer)
+            rule_executor = self._rule_registry.get_executor(rule.type.upper())
+            if rule_executor is None:
+                self._run_action(ctx, rule_mode, rule, rule.on_failure, message,
+                                 RuleError(f"Could not find rule executor of type {rule.type}"),
+                                 'ERROR')
+                return message
+            try:
+                result = rule_executor.transform(ctx, message)
+                if rule.kind == RuleKind.CONDITION:
+                    if not result:
+                        raise RuleConditionError(rule)
+                    break
+                elif rule.kind == RuleKind.TRANSFORM:
+                    message = result
+                    break
+                self._run_action(ctx, rule_mode, rule,
+                                 rule.on_failure if message is None else rule.on_success,
+                                 message, None,
+                                 'ERROR' if message is None else 'NONE')
+            except SerializationError:
+                raise
+            except Exception as e:
+                self._run_action(ctx, rule_mode, rule, rule.on_failure, message, e, 'ERROR')
         return message
+
+    def _run_action(self, ctx: RuleContext, rule_mode: RuleMode, rule: Rule, action: str, message: object,
+        ex: Exception, default_action: str):
+        action_name = self._get_rule_action_name(rule, rule_mode, action)
+        if action_name is None:
+            action_name = default_action
+        rule_action = self._get_rule_action(self._rule_registry, action_name)
+        if rule_action is None:
+            raise RuleError(f"Could not find rule action of type {action_name}")
+        try:
+            rule_action.run(ctx, message, ex)
+        except SerializationError:
+            raise
+        except Exception as e:
+            log.warning(f"Could not run post-rule action {action_name}: {e}")
+
+    def _get_rule_action_name(self, rule: Rule, rule_mode: RuleMode, action_name: str) -> Optional[str]:
+        if action_name is None or action_name == "":
+            return None
+        if (rule.mode == RuleMode.WRITEREAD or rule.mode == RuleMode.UPDOWN) and ',' in action_name:
+            parts = action_name.split(',')
+            if rule_mode == RuleMode.WRITE or rule_mode == RuleMode.UPGRADE:
+                return parts[0]
+            elif rule_mode == RuleMode.READ or rule_mode == RuleMode.DOWNGRADE:
+                return parts[1]
+        return action_name
+
+    def _get_rule_action(self, ctx: RuleContext, action_name: str) -> RuleAction:
+        if action_name == 'ERROR':
+            return ErrorAction()
+        elif action_name == 'NONE':
+            return NoneAction()
+        return self._rule_registry.get_action(action_name)
 
 
 class BaseSerializer(BaseSerde, Serializer):
