b7003f8825852bef670518c5dfe99a4eec45a20b
Pranav Rathi <4427674+pranavrth@users.noreply.github.com>
2025-01-07 22:03:13 +0530

    Change for v2.8.0 main release

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = u'2016-2024, Confluent Inc.'
 # built documents.
 #
 # The short X.Y version.
-version = '2.8.0rc1'
+version = '2.8.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 ######################################################################
