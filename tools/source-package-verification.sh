b2faa89366715ed8fcc5e52462ee0a5dce54d6d5
Robert Yokota <rayokota@gmail.com>
2025-01-09 09:12:20 -0800

    Use poetry

--- tools/source-package-verification.sh
+++ tools/source-package-verification.sh
@@ -5,9 +5,6 @@
 #
 set -e
 
-pip install poetry
-poetry install --all-extras 
-pip install tests/trivup/trivup-0.12.7.tar.gz
 pip install -U build
 
 lib_dir=dest/runtimes/$OS_NAME-$ARCH/native
@@ -17,7 +14,9 @@ export LDFLAGS="$LDFLAGS -L${PWD}/${lib_dir}"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PWD/$lib_dir"
 export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$PWD/$lib_dir"
 
-python3 -m pip install .
+pip install poetry
+poetry install --all-extras 
+pip install tests/trivup/trivup-0.12.7.tar.gz
 
 if [[ $OS_NAME == linux && $ARCH == x64 ]]; then
     if [[ -z $TEST_CONSUMER_GROUP_PROTOCOL ]]; then
