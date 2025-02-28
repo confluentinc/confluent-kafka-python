790bc8742af13d31bc892d7096c5f4a74938b53f
Justin Wang <35274216+Claimundefine@users.noreply.github.com>
2025-02-27 20:44:59 -0500

    Changes for v2.8.1 release (#1926)
    * Version v2.8.1rc1
    
    * Add changelog for v2.8.1 release
    
    * Increase spacing

--- src/confluent_kafka/src/confluent_kafka.h
+++ src/confluent_kafka/src/confluent_kafka.h
@@ -42,8 +42,8 @@
  *  0xMMmmRRPP
  *  MM=major, mm=minor, RR=revision, PP=patchlevel (not used)
  */
-#define CFL_VERSION     0x02080000
-#define CFL_VERSION_STR "2.8.0"
+#define CFL_VERSION     0x02080100
+#define CFL_VERSION_STR "2.8.1"
 
 /**
  * Minimum required librdkafka version. This is checked both during
