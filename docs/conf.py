b578e920e5de4acb7f6a33c5a97e93debb7da3fb
claimundefine <juswang@confluent.io>
2025-02-28 19:14:43 -0500

    Version v2.8.2rc1

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = u'2016-2024, Confluent Inc.'
 # built documents.
 #
 # The short X.Y version.
-version = '2.8.1'
+version = '2.8.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 ######################################################################
