7c28c32aefbd257750b68d10f49ce6704cad90cf
Robert Yokota <rayokota@gmail.com>
2025-02-28 23:33:11 -0800

    Add test with record refs

--- tests/schema_registry/test_avro_serdes.py
+++ tests/schema_registry/test_avro_serdes.py
@@ -247,6 +247,45 @@ def test_avro_serialize_union():
     assert obj == obj2
 
 
+def test_avro_serialize_union_with_record_references():
+    conf = {'url': _BASE_URL}
+    client = SchemaRegistryClient.new_client(conf)
+    ser_conf = {'auto.register.schemas': False, 'use.latest.version': True}
+
+    obj = {
+        'First': {'stringField': 'hi'},
+        'Second': {'stringField': 'hi'},
+    }
+    ref_schema = {
+        'type': 'record',
+        'namespace': 'test',
+        'name': 'B',
+        'fields': [
+            {'name': 'stringField', 'type': 'string'},
+        ]
+    }
+    client.register_schema('ref', Schema(json.dumps(ref_schema)))
+    schema = ['null', {
+        'type': 'record',
+        'name': 'A',
+        'namespace': 'test',
+        'fields': [
+            {'name': 'First', 'type': 'B'},
+            {'name': 'Second', 'type': 'B'}
+        ]
+    }]
+    refs = [SchemaReference('test.B', 'ref', 1)]
+    client.register_schema(_SUBJECT, Schema(json.dumps(schema), 'AVRO', refs))
+
+    ser = AvroSerializer(client, schema_str=None, conf=ser_conf)
+    ser_ctx = SerializationContext(_TOPIC, MessageField.VALUE)
+    obj_bytes = ser(obj, ser_ctx)
+
+    deser = AvroDeserializer(client)
+    obj2 = deser(obj_bytes, ser_ctx)
+    assert obj == obj2
+
+
 def test_avro_serialize_union_with_references():
     conf = {'url': _BASE_URL}
     client = SchemaRegistryClient.new_client(conf)
