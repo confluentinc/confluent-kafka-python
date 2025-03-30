be03e8fe41211fbbfada10dfaef7330437235649
Pranav Rathi <4427674+pranavrth@users.noreply.github.com>
2025-03-31 03:15:55 +0530

    Added consumer close in a separate thread.

--- tests/chaos-test.py
+++ tests/chaos-test.py
@@ -5,6 +5,9 @@ from confluent_kafka.admin import AdminClient, NewTopic
 import random
 import signal
 import sys
+import threading
+
+consumer_stopped = False
 
 def wait_for_containers_to_be_healthy(no_of_containers):
     # Run 'docker-compose up -d' in detached mode
@@ -70,13 +73,15 @@ def stop_all_kafka_containers(all_kafka_containers, stopped_container):
 
 def reset_or_create_consumer(brokers, consumer, topic_name):
     time.sleep(0.05)
-    stop_consumer = random.choice([True, False, False])
+    stop_consumer = random.choice([True])
     if stop_consumer and consumer:
+        consumer_stopped = True
         print("Closing Kafka consumer")
         consumer.close()
         consumer = None
     if not consumer:
         # Create a Kafka consumer
+        consumer_stopped = False
         consumer = Consumer({
             "bootstrap.servers": brokers,
             "group.id": topic_name,
@@ -94,16 +99,18 @@ def start_all_kafka_containers(all_kafka_containers, stopped_container):
             stopped_container.remove(container)
 
 def start_stop_broker_consumer(all_kafka_containers, stopped_container):
-    control = random.randint(1, 399)
-    if control == 100:
+    frequency = 5
+    control = random.randint(1, 100*frequency)
+    control -= 1
+    if control == 0:
         stop_container(all_kafka_containers, stopped_container)
-    elif control == 0:
+    elif control == 25*frequency:
         start_stopped_kafka_container(stopped_container)
-    elif control == 200:
+    if control == 50*frequency:
         stop_all_kafka_containers(all_kafka_containers, stopped_container)
-    elif control == 300:
+    elif control == 75*frequency:
         start_all_kafka_containers(all_kafka_containers, stopped_container)
-    return control % 100 == 0
+    return control % (25*frequency) == 0
 
 def create_topics(brokers, topics, partitions=3, rep=1):
     a = AdminClient({'bootstrap.servers': brokers})
@@ -142,37 +149,58 @@ def main():
     # Handle SIGINT
     def signal_handler(sig, frame):
         print("You pressed Ctrl+C! Exiting gracefully...")
+
         time.sleep(5)
         print(f"Stopped containers: {stopped_container}")
         while len(stopped_container) > 0:
             start_stopped_kafka_container(stopped_container)
-        time.sleep(5)
+
+        time.sleep(10)
+
         if consumer:
             print("Closing Kafka consumer")
             consumer.close()
+
         print("Flushing Kafka producer")
         producer.flush()
+
+        print("Deleting Kafka topic")
+        a = AdminClient({'bootstrap.servers': brokers})
+        delete_topics = a.delete_topics([topic_name])
+        for topic, f in delete_topics.items():
+            try:
+                f.result()  # The result itself is None
+                print("Topic {} deleted".format(topic))
+            except Exception as e:
+                print("Failed to delete topic {}: {}".format(topic, e))
+
         exit(0)
+
     signal.signal(signal.SIGINT, signal_handler)
 
     while True:
         reset_consumer = start_stop_broker_consumer(all_kafka_containers, stopped_container)
-        if reset_consumer:
-            consumer = reset_or_create_consumer(brokers, consumer, topic_name)
+        if reset_consumer and not consumer_stopped:
+            def reset_consumer_thread():
+                nonlocal consumer
+                consumer = reset_or_create_consumer(brokers, consumer, topic_name)
+            threading.Thread(target=reset_consumer_thread).start()
+
+        if not consumer_stopped:
+            # Consume a message
+            message = consumer.poll(timeout=0.001)
+            if message is None:
+                print("No message received")
+            else:
+                print(f"Received message: {message.value()}")
+
 
         message_to_produce=f"value-{random.randint(1, 1000)}"
-
         if random.choice([True, False, False, False, False]):
             # Produce a message
             producer.produce(topic_name, value=message_to_produce)
             print(f"Producing message: {message_to_produce}")
 
-        # Consume a message
-        message = consumer.poll(timeout=0.001)
-        if message is None:
-            print("No message received")
-        else:
-            print(f"Received message: {message.value()}")
         time.sleep(0.2)
 
 if __name__ == "__main__":
