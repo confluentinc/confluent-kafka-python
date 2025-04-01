bd1aaa48db7dac51ee55f4bb66e7b8c545adc183
Pranav Rathi <4427674+pranavrth@users.noreply.github.com>
2025-04-01 16:21:32 +0530

    Few improvements and added color coding

--- tests/chaos-test.py
+++ tests/chaos-test.py
@@ -9,6 +9,49 @@ import threading
 
 consumer_stopped = False
 
+class Logger:
+    max_logs = 10000
+
+    def __init__(self):
+        self.pos = 0
+        self.buffer = [None] * Logger.max_logs
+        self.do_log = False
+
+    def show(self):
+        if self.do_log:
+            return
+
+        i = self.pos
+        while ((i + 1) % Logger.max_logs) != self.pos:
+            if self.buffer[i] is not None:
+                print(self.buffer[i])
+                self.buffer[i] = None
+            i = (i + 1) % Logger.max_logs
+        if self.buffer[i] is not None:
+            print(self.buffer[i])
+        self.do_log = True
+    
+    def hide(self):
+        self.do_log = False
+
+    def log(self, level, format, facet, name, *args):
+        self.buffer[self.pos] = f"{time.time()} {name} {facet}: {" ".join(args)}"
+        if self.do_log:
+            print(self.buffer[self.pos])
+        self.pos = (self.pos + 1) % Logger.max_logs
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def wait_for_containers_to_be_healthy(no_of_containers):
     # Run 'docker-compose up -d' in detached mode
     subprocess.run(["docker", "compose", "up", "-d"], check=True)
@@ -46,12 +89,12 @@ def start_kafka_container(container_name):
     subprocess.Popen(["docker", "start", container_name])
 
 def start_stopped_kafka_container(stopped_container):
-    print(f"Start Container: Current stopped containers: {stopped_container}")
+    print(f"{bcolors.OKBLUE}Start Container: Current stopped containers: {stopped_container}{bcolors.ENDC}")
     if len(stopped_container) > 0:
         starting_container = random.choice(stopped_container)
         stopped_container.remove(starting_container)
         start_kafka_container(starting_container)
-        print(f"Started Kafka container: {starting_container}")
+        print(f"{bcolors.OKBLUE}Started Kafka container: {starting_container}{bcolors.ENDC}")
 
 def stop_container(all_kafka_containers, stopped_container):
     random_kafka_container = random.choice(list(all_kafka_containers.keys()))
@@ -59,20 +102,20 @@ def stop_container(all_kafka_containers, stopped_container):
     if stopping_container not in stopped_container:
         stop_kafka_container(stopping_container)
         stopped_container.append(stopping_container)
-        print(f"Stopped Kafka container: {stopping_container}")
+        print(f"{bcolors.WARNING}Stopped Kafka container: {stopping_container}{bcolors.ENDC}")
     else:
         print(f"Kafka container {stopping_container} is already stopped")
-    print(f"Stop Container: Current stopped containers: {stopped_container}")
+    print(f"{bcolors.WARNING}Stop Container: Current stopped containers: {stopped_container}{bcolors.ENDC}")
 
 def stop_all_kafka_containers(all_kafka_containers, stopped_container):
-    print("Stopping all Kafka containers")
+    print(f"{bcolors.FAIL}Stopping all Kafka containers{bcolors.ENDC}")
     for container in all_kafka_containers.values():
         stop_kafka_container(container)
         if container not in stopped_container:
             stopped_container.append(container)
 
 def reset_or_create_consumer(brokers, consumer, topic_name):
-    time.sleep(0.05)
+    time.sleep(0.005)
     stop_consumer = random.choice([True])
     if stop_consumer and consumer:
         consumer_stopped = True
@@ -92,7 +135,7 @@ def reset_or_create_consumer(brokers, consumer, topic_name):
     return consumer
 
 def start_all_kafka_containers(all_kafka_containers, stopped_container):
-    print("Starting all Kafka containers")
+    print(f"{bcolors.OKGREEN}Starting all Kafka containers{bcolors.ENDC}")
     for container in all_kafka_containers.values():
         start_kafka_container(container)
         if container in stopped_container:
@@ -123,6 +166,11 @@ def create_topics(brokers, topics, partitions=3, rep=1):
         except Exception as e:
             print("Failed to create topic {}: {}".format(topic, e))
 
+# def delivery_cb(err, msg):
+#     if err is not None:
+#         print(bcolors.BOLD +"Message delivery failed: {}".format(err) + bcolors.ENDC)
+#     else:
+#         print(bcolors.BOLD + "Message delivered to {} [{}]".format(msg.topic(), msg.partition()) + bcolors.ENDC)
 
 def main():
     brokers = sys.argv[1]
@@ -134,13 +182,16 @@ def main():
     wait_for_containers_to_be_healthy(int(sys.argv[2]))
     all_kafka_containers = get_kafka_container()
 
+    print("Waiting for few seconds before starting consumer and producer...")
+    time.sleep(5)
+
     # Create a Kafka topic
     create_topics(brokers, [topic_name], 12, 1)
 
     # Create a Kafka producer
     producer = Producer({
         "bootstrap.servers": brokers,
-        "linger.ms": 0
+        "linger.ms": 0,
     })
 
     # Create a Kafka consumer
@@ -178,20 +229,29 @@ def main():
 
     signal.signal(signal.SIGINT, signal_handler)
 
+    looping = 0
+    print_no_message = 1
     while True:
         reset_consumer = start_stop_broker_consumer(all_kafka_containers, stopped_container)
-        if reset_consumer and not consumer_stopped:
-            def reset_consumer_thread():
-                nonlocal consumer
-                consumer = reset_or_create_consumer(brokers, consumer, topic_name)
-            threading.Thread(target=reset_consumer_thread).start()
+        # if reset_consumer and not consumer_stopped:
+        #     def reset_consumer_thread():
+        #         nonlocal consumer
+        #         consumer = reset_or_create_consumer(brokers, consumer, topic_name)
+        #     threading.Thread(target=reset_consumer_thread).start()
+        if reset_consumer:
+            consumer = reset_or_create_consumer(brokers, consumer, topic_name)
 
         if not consumer_stopped:
             # Consume a message
             message = consumer.poll(timeout=0.001)
             if message is None:
-                print("No message received")
+                if print_no_message % 10 == 0:
+                    print_no_message = 1
+                    print("No message received")
+                else:
+                    print_no_message += 1
             else:
+                print_no_message = 1
                 print(f"Received message: {message.value()}")
 
 
@@ -200,6 +260,11 @@ def main():
             # Produce a message
             producer.produce(topic_name, value=message_to_produce)
             print(f"Producing message: {message_to_produce}")
+        # producer.poll(0)
+
+        if looping % 100 == 0:
+            print("loop going on")
+        looping += 1
 
         time.sleep(0.2)
 
