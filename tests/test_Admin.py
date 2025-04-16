ba5eb746b7d09753214760bf2eb607167704cf02
Pranav Rathi <4427674+pranavrth@users.noreply.github.com>
2025-04-17 03:27:22 +0530

    Trying to fix MacOS build fatal error issue

--- tests/test_Admin.py
+++ tests/test_Admin.py
@@ -835,8 +835,8 @@ def test_alter_consumer_group_offsets_api():
         a.alter_consumer_group_offsets([request_with_group_and_topic_partition_offset1,
                                         same_name_request])
 
-    fs = a.alter_consumer_group_offsets([request_with_group_and_topic_partition_offset1])
     with pytest.raises(KafkaException):
+        fs = a.alter_consumer_group_offsets([request_with_group_and_topic_partition_offset1])
         for f in fs.values():
             f.result(timeout=10)
 
