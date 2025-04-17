4799d921cf2278b8e02cb6d5c4df377d3393b0b6
Rohit Sanjay <rsanjay@confluent.io>
2025-04-17 14:49:03 -0700

    Refactor for async support

--- src/confluent_kafka/schema_registry/_sync/__init__.py
+++ src/confluent_kafka/schema_registry/_sync/__init__.py
@@ -14,7 +14,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from .common.avro import *
-from ..avro import *
-from ._sync.avro import *
+#
