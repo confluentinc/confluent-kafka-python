a1de9b1b2add876dc8432499686245e61e26d9c2
Emanuele Sabellico <esabellico@confluent.io>
2025-07-21 22:18:00 +0200

    Add otel collector installation to the bootstrap script

--- tests/soak/bootstrap.sh
+++ tests/soak/bootstrap.sh
@@ -16,11 +16,20 @@ fi
 
 python_branch=$1
 librdkafka_branch=$2
+otel_collector_version=0.130.0
+otel_collector_package_url="https://github.com/open-telemetry/"\
+"opentelemetry-collector-releases/releases/download/"\
+"v${otel_collector_version}/otelcol-contrib_${otel_collector_version}_linux_amd64.deb"
 venv=$PWD/venv
 
 sudo apt update
-sudo apt install -y git curl make gcc g++ zlib1g-dev libssl-dev libzstd-dev \
-    python3-dev python3-pip python3-venv
+sudo apt install -y git curl wget make gcc g++ zlib1g-dev libssl-dev \
+    libzstd-dev python3-dev python3-pip python3-venv
+wget -O otel_collector_package.deb $otel_collector_package_url
+sudo dpkg -i otel_collector_package.deb
+rm otel_collector_package.deb
+sudo cp otel-config.yaml /etc/otelcol-contrib/config.yaml
+sudo systemctl restart otelcol-contrib
 
 if [[ ! -d confluent-kafka-python ]]; then
     git clone https://github.com/confluentinc/confluent-kafka-python
