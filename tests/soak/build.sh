bd252adf5467c46bf5148f7b02750beb10ddb0bc
Emanuele Sabellico <esabellico@confluent.io>
2025-07-22 15:31:16 +0200

    Use a local librdkafka installation different for every started test Use test code from the checked out branch instead of the selected CKPy version Bootstrap the same way as when changing version: with `build.sh`

--- tests/soak/build.sh
+++ tests/soak/build.sh
@@ -11,28 +11,50 @@ fi
 
 set -eu
 
+testdir=$PWD
+mkdir -p $testdir/librdkafka-installation
 
+if [[ ! -d confluent-kafka-python ]]; then
+    git clone https://github.com/confluentinc/confluent-kafka-python
+fi
+
+venv=$PWD/venv
+if [[ ! -d $venv ]]; then
+    echo "Setting up virtualenv in $venv"
+    python3 -m venv $venv
+    source $venv/bin/activate
+    pip install -U pip
+    pip install -r $testdir/../../requirements/requirements-soaktest.txt
+    deactivate
+fi
 
 echo "Building and installing librdkafka $librdkafka_version"
 if [[ ! -d librdkafka ]]; then
     git clone https://github.com/confluentinc/librdkafka.git
 fi
 pushd librdkafka
-sudo make uninstall
 git fetch --tags
 git checkout $librdkafka_version
-./configure --reconfigure
+echo "Configuring librdkafka $librdkafka_version with prefix $testdir/librdkafka-installation"
+./configure --prefix=$testdir/librdkafka-installation
+sudo make uninstall
 make clean
 make -j
-sudo make install
+make install
 popd
 
+export LIBRARY_PATH=$testdir/librdkafka-installation/lib
+export LD_LIBRARY_PATH=$testdir/librdkafka-installation/lib
+export CPLUS_INCLUDE_PATH=$testdir/librdkafka-installation/include
+export C_INCLUDE_PATH=$testdir/librdkafka-installation/include
 
 echo "Building confluent-kafka-python $cflpy_version"
 set +u
 source venv/bin/activate
+python3 -m pip uninstall -y confluent-kafka
 set -u
 pushd confluent-kafka-python
+rm -rf ./build
 git fetch --tags
 git checkout $cflpy_version
 python3 -m pip install .
@@ -41,4 +63,3 @@ popd
 echo ""
 echo "=============================================================================="
 (cd / ; python3 -c 'import confluent_kafka as c; print("python", c.version(), "librdkafka", c.libversion())')
-
