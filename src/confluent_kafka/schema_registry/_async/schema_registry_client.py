765795cd078b0b653486fad41a12d41a95d4ed7c
Naxin Fang <nfang@confluent.io>
2025-08-11 12:10:48 -0400

    Migrate to orjson (JSON library) for better performance  (#2016)
    * update
    
    * unicode
    
    * update

--- src/confluent_kafka/schema_registry/_async/schema_registry_client.py
+++ src/confluent_kafka/schema_registry/_async/schema_registry_client.py
@@ -16,7 +16,7 @@
 # limitations under the License.
 #
 import asyncio
-import json
+import orjson
 import logging
 import time
 import urllib
@@ -416,7 +416,7 @@ class _AsyncRestClient(_AsyncBaseRestClient):
                              " application/json"}
 
         if body is not None:
-            body = json.dumps(body)
+            body = orjson.dumps(body).decode('utf-8')
             headers = {'Content-Length': str(len(body)),
                        'Content-Type': "application/vnd.schemaregistry.v1+json"}
 
