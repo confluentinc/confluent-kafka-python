866b9708a88727d8447318168c36e47641c08599
Emanuele Sabellico <esabellico@confluent.io>
2025-08-18 22:39:54 +0200

    2.11.1 (#2023)

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = u'2016-2025, Confluent Inc.'
 # built documents.
 #
 # The short X.Y version.
-version = '2.11.1rc2'
+version = '2.11.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 ######################################################################
--- src/confluent_kafka/src/confluent_kafka.h
+++ src/confluent_kafka/src/confluent_kafka.h
@@ -43,7 +43,7 @@
  *  MM=major, mm=minor, RR=revision, PP=patchlevel (not used)
  */
 #define CFL_VERSION     0x020b0100
-#define CFL_VERSION_STR "2.11.1rc2"
+#define CFL_VERSION_STR "2.11.1"
 
 /**
  * Minimum required librdkafka version. This is checked both during
