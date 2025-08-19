ff574c0064c6a0dcc437a7fc1361d30adc749c0b
Emanuele Sabellico <esabellico@confluent.io>
2025-08-19 19:26:44 +0200

    PEP8 compliance

--- tests/soak/setup_all_versions.py
+++ tests/soak/setup_all_versions.py
@@ -4,31 +4,32 @@ import subprocess
 
 PYTHON_SOAK_TEST_BRANCH = 'dev_otel_agent_soak_test'
 
-LIBRDKAFKA_VERSIONS=[
-  #'2.11.1'
-  #'2.10.1',
-  '2.8.0',
-  '2.6.1',
-  '2.5.3',
-  '2.4.0',
-  '2.3.0',
-  '2.2.0',
-  '2.1.1',
-  '2.0.2'
+LIBRDKAFKA_VERSIONS = [
+    # '2.11.1'
+    # '2.10.1',
+    '2.8.0',
+    '2.6.1',
+    '2.5.3',
+    '2.4.0',
+    '2.3.0',
+    '2.2.0',
+    '2.1.1',
+    '2.0.2'
 ]
 
-PYTHON_VERSIONS=[
-  #'2.11.0'
-  #'2.10.1',
-  '2.8.0',
-  '2.6.1',
-  '2.5.3',
-  '2.4.0',
-  '2.3.0',
-  '2.2.0',
-  '2.1.1',
-  '2.0.2'
+PYTHON_VERSIONS = [
+    # '2.11.0'
+    # '2.10.1',
+    '2.8.0',
+    '2.6.1',
+    '2.5.3',
+    '2.4.0',
+    '2.3.0',
+    '2.2.0',
+    '2.1.1',
+    '2.0.2'
 ]
+
 
 def run(command):
     print(f'Running command: {command}')
@@ -38,15 +39,17 @@ def run(command):
     if result.returncode != 0:
         raise Exception(f'Command failed: {command}, result: {result.returncode}')
 
+
 if __name__ == '__main__':
     for i, librdkafka_version in enumerate(LIBRDKAFKA_VERSIONS):
         python_version = PYTHON_VERSIONS[i]
-        folder_name = f'confluent-kafka-python_{librdkafka_version.replace('.', '_')}'
+        librdkafka_version_folder_name = librdkafka_version.replace('.', '_')
+        folder_name = f'confluent-kafka-python_{librdkafka_version_folder_name}'
         if os.path.exists(folder_name):
             print(f'Skipping \'{folder_name}\', already exists.')
             continue
 
-        run(f'git clone https://github.com/confluentinc/confluent-kafka-python.git')
+        run('git clone https://github.com/confluentinc/confluent-kafka-python.git')
         run(f'mv confluent-kafka-python {folder_name}')
         run(f'cd {folder_name} && git checkout {PYTHON_SOAK_TEST_BRANCH}')
         run(f'cd {folder_name}/tests/soak && ./build.sh v{librdkafka_version} v{python_version}')
