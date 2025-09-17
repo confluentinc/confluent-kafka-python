66186581d3868b2db1eb8f91c6c3653a1a6f9a24
Emanuele Sabellico <esabellico@confluent.io>
2025-09-17 20:11:58 +0200

    Keep last two log files of 50MB max

--- tests/soak/run.sh
+++ tests/soak/run.sh
@@ -12,24 +12,42 @@ if [[ -z $librdkafka_version ]]; then
 fi
 
 set -u
+run=true
 topic="pysoak-$TESTID-$librdkafka_version"
 logfile="${TESTID}.log.bz2"
+limit=$((50 * 1024 * 1024)) # 50MB
 export HOSTNAME=$(hostname)
 echo "Starting soak client using topic $topic. Logging to $logfile."
 set +x
-# Ignore SIGINT in children (inherited)
-trap "" SIGINT
-time opentelemetry-instrument $testdir/soakclient.py -i $TESTID -t $topic -r 80 -f $1 |& tee /dev/tty | bzip2 > $logfile &
-PID=$!
-# On SIGINT kill only the first process of the pipe
-onsigint() {
+while [ "$run" = true ]; do
+    # Ignore SIGINT in children (inherited)
+    trap "" SIGINT
+    time opentelemetry-instrument $testdir/soakclient.py -i $TESTID -t $topic -r 80 -f $1 |& tee /dev/tty | bzip2 > $logfile &
+    PID=$!
+    terminate_last() {
         # List children of $PID only
         ps --ppid $PID -f | grep soakclient.py | grep -v grep | awk '{print $2}' | xargs kill
-}
-trap onsigint SIGINT
-# Await the result
-wait $PID
-ret=$?
-echo "Python client exited with status $ret"
+    }
+    # On SIGINT kill only the first process of the pipe
+    onsigint() {
+        echo "Terminating soak client using topic $topic. Logging to $logfile."
+        terminate_last
+        run=false
+    }
+    trap onsigint SIGINT
+    # Await the result
+    sleep 1
+    size=$(stat -c%s "$logfile")
+    while (( size < limit )) && [ "$run" = true ]; do
+        sleep 1
+        size=$(stat -c%s "$logfile")
+    done
+    echo "Rolling log file: $logfile"
+    terminate_last
+    wait $PID
+    ret=$?
+    echo "Python client exited with status $ret"
+    mv $logfile "${TESTID}.log.prev.bz2" || true
+done
 echo "Ending soak client using topic $topic. Logging to $logfile."
 exit $ret
