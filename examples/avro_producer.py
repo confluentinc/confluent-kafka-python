a7622874df6634c7ccf74fb109bff79d14da5ab8
Kaushik Raina <kraina@confluent.io>
2025-10-29 19:20:59 +0530

    Update schema registry example to use with clause

--- examples/avro_producer.py
+++ examples/avro_producer.py
@@ -114,43 +114,42 @@ def main(args):
     # If Confluent Cloud SR credentials are provided, add to config
     if args.sr_api_key and args.sr_api_secret:
         schema_registry_conf['basic.auth.user.info'] = f"{args.sr_api_key}:{args.sr_api_secret}"
-    schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+    
+    # Use context manager for SchemaRegistryClient to ensure proper cleanup
+    with SchemaRegistryClient(schema_registry_conf) as schema_registry_client:
+        avro_serializer = AvroSerializer(schema_registry_client,
+                                         schema_str,
+                                         user_to_dict)
 
-    avro_serializer = AvroSerializer(schema_registry_client,
-                                     schema_str,
-                                     user_to_dict)
+        string_serializer = StringSerializer('utf_8')
 
-    string_serializer = StringSerializer('utf_8')
+        producer_conf = {'bootstrap.servers': args.bootstrap_servers}
 
-    producer_conf = {'bootstrap.servers': args.bootstrap_servers}
-
-    producer = Producer(producer_conf)
-
-    print("Producing user records to topic {}. ^C to exit.".format(topic))
-    while True:
-        # Serve on_delivery callbacks from previous calls to produce()
-        producer.poll(0.0)
-        try:
-            user_name = input("Enter name: ")
-            user_address = input("Enter address: ")
-            user_favorite_number = int(input("Enter favorite number: "))
-            user_favorite_color = input("Enter favorite color: ")
-            user = User(name=user_name,
-                        address=user_address,
-                        favorite_color=user_favorite_color,
-                        favorite_number=user_favorite_number)
-            producer.produce(topic=topic,
-                             key=string_serializer(str(uuid4())),
-                             value=avro_serializer(user, SerializationContext(topic, MessageField.VALUE)),
-                             on_delivery=delivery_report)
-        except KeyboardInterrupt:
-            break
-        except ValueError:
-            print("Invalid input, discarding record...")
-            continue
-
-    print("\nFlushing records...")
-    producer.flush()
+        # Use context manager for Producer to ensure proper cleanup
+        with Producer(producer_conf) as producer:
+            print("Producing user records to topic {}. ^C to exit.".format(topic))
+            while True:
+                # Serve on_delivery callbacks from previous calls to produce()
+                producer.poll(0.0)
+                try:
+                    user_name = input("Enter name: ")
+                    user_address = input("Enter address: ")
+                    user_favorite_number = int(input("Enter favorite number: "))
+                    user_favorite_color = input("Enter favorite color: ")
+                    user = User(name=user_name,
+                                address=user_address,
+                                favorite_color=user_favorite_color,
+                                favorite_number=user_favorite_number)
+                    producer.produce(topic=topic,
+                                     key=string_serializer(str(uuid4())),
+                                     value=avro_serializer(user, SerializationContext(topic, MessageField.VALUE)),
+                                     on_delivery=delivery_report)
+                except KeyboardInterrupt:
+                    break
+                except ValueError:
+                    print("Invalid input, discarding record...")
+                    continue
+            # Producer will automatically flush on context exit
 
 
 if __name__ == '__main__':
