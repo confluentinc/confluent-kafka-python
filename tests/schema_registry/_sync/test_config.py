8b50cb83b01566f086256ab60ee8d9862895dee2
claimundefine <juswang@confluent.io>
2026-02-16 22:40:25 -0500

    Add UAMI OAuth changes

--- tests/schema_registry/_sync/test_config.py
+++ tests/schema_registry/_sync/test_config.py
@@ -19,7 +19,8 @@ import pytest
 from httpx import BasicAuth
 
 from confluent_kafka.schema_registry import SchemaRegistryClient
-from confluent_kafka.schema_registry.rules.encryption.encrypt_executor import FieldEncryptionExecutor
+from confluent_kafka.schema_registry.rules.encryption.encrypt_executor import \
+    FieldEncryptionExecutor
 from confluent_kafka.schema_registry.serde import RuleError
 
 TEST_URL = 'http://SchemaRegistry:65534'
@@ -45,13 +46,15 @@ def test_config_url_invalid():
 
 def test_config_url_invalid_type():
     conf = {'url': dict()}
-    with pytest.raises(TypeError, match="url must be a str," " not <(.*)>$"):
+    with pytest.raises(TypeError, match="url must be a str,"
+                                        " not <(.*)>$"):
         SchemaRegistryClient(conf)
 
 
 def test_config_url_none():
     conf = {}
-    with pytest.raises(ValueError, match="Missing required configuration" " property url"):
+    with pytest.raises(ValueError, match="Missing required configuration"
+                                         " property url"):
         SchemaRegistryClient(conf)
 
 
@@ -62,196 +65,264 @@ def test_config_url_trailing_slash():
 
 
 def test_config_ssl_key_no_certificate():
-    conf = {'url': TEST_URL, 'ssl.key.location': '/ssl/keys/client'}
-    with pytest.raises(
-        ValueError,
-        match="ssl.certificate.location required" " when configuring ssl.key.location" " or ssl.key.password",
-    ):
+    conf = {'url': TEST_URL,
+            'ssl.key.location': '/ssl/keys/client'}
+    with pytest.raises(ValueError, match="ssl.certificate.location required"
+                                         " when configuring ssl.key.location"
+                                         " or ssl.key.password"):
         SchemaRegistryClient(conf)
 
 
 def test_config_ssl_password_no_certificate():
-    conf = {'url': TEST_URL, 'ssl.key.password': 'sesame'}
-    with pytest.raises(
-        ValueError,
-        match="ssl.certificate.location required" " when configuring ssl.key.location" " or ssl.key.password",
-    ):
+    conf = {'url': TEST_URL,
+            'ssl.key.password': 'sesame'}
+    with pytest.raises(ValueError, match="ssl.certificate.location required"
+                                         " when configuring ssl.key.location"
+                                         " or ssl.key.password"):
         SchemaRegistryClient(conf)
 
 
 def test_config_auth_url():
-    conf = {'url': 'http://' + TEST_USERNAME + ":" + TEST_USER_PASSWORD + '@SchemaRegistry:65534'}
+    conf = {
+        'url': 'http://'
+               + TEST_USERNAME + ":"
+               + TEST_USER_PASSWORD + '@SchemaRegistry:65534'}
     test_client = SchemaRegistryClient(conf)
-    assert (
-        test_client._rest_client.session.auth._auth_header == BasicAuth(TEST_USERNAME, TEST_USER_PASSWORD)._auth_header
-    )
+    assert (test_client._rest_client.session.auth._auth_header ==
+            BasicAuth(TEST_USERNAME, TEST_USER_PASSWORD)._auth_header)
 
 
 def test_config_auth_url_and_userinfo():
     conf = {
-        'url': 'http://' + TEST_USERNAME + ":" + TEST_USER_PASSWORD + '@SchemaRegistry:65534',
+        'url': 'http://'
+               + TEST_USERNAME + ":"
+               + TEST_USER_PASSWORD + '@SchemaRegistry:65534',
         'basic.auth.credentials.source': 'user_info',
-        'basic.auth.user.info': TEST_USERNAME + ":" + TEST_USER_PASSWORD,
-    }
+        'basic.auth.user.info': TEST_USERNAME + ":" + TEST_USER_PASSWORD}
 
-    with pytest.raises(
-        ValueError,
-        match="basic.auth.user.info configured with"
-        " userinfo credentials in the URL."
-        " Remove userinfo credentials from the"
-        " url or remove basic.auth.user.info"
-        " from the configuration",
-    ):
+    with pytest.raises(ValueError, match="basic.auth.user.info configured with"
+                                         " userinfo credentials in the URL."
+                                         " Remove userinfo credentials from the"
+                                         " url or remove basic.auth.user.info"
+                                         " from the configuration"):
         SchemaRegistryClient(conf)
 
 
 def test_config_auth_userinfo():
-    conf = {'url': TEST_URL, 'basic.auth.user.info': TEST_USERNAME + ':' + TEST_USER_PASSWORD}
+    conf = {'url': TEST_URL,
+            'basic.auth.user.info': TEST_USERNAME + ':' + TEST_USER_PASSWORD}
 
     test_client = SchemaRegistryClient(conf)
-    assert (
-        test_client._rest_client.session.auth._auth_header == BasicAuth(TEST_USERNAME, TEST_USER_PASSWORD)._auth_header
-    )
+    assert (test_client._rest_client.session.auth._auth_header ==
+            BasicAuth(TEST_USERNAME, TEST_USER_PASSWORD)._auth_header)
 
 
 def test_config_auth_userinfo_invalid():
-    conf = {'url': TEST_URL, 'basic.auth.user.info': 'lookmanocolon'}
+    conf = {'url': TEST_URL,
+            'basic.auth.user.info': 'lookmanocolon'}
 
-    with pytest.raises(ValueError, match="basic.auth.user.info must be in the" " form of {username}:{password}$"):
+    with pytest.raises(ValueError, match="basic.auth.user.info must be in the"
+                                         " form of {username}:{password}$"):
         SchemaRegistryClient(conf)
 
 
 def test_bearer_config():
-    conf = {'url': TEST_URL, 'bearer.auth.credentials.source': "OAUTHBEARER"}
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': "OAUTHBEARER"}
 
     with pytest.raises(ValueError, match=r"Missing required bearer configuration properties: (.*)"):
         SchemaRegistryClient(conf)
 
 
 def test_oauth_bearer_config_missing():
-    conf = {
-        'url': TEST_URL,
-        'bearer.auth.credentials.source': "OAUTHBEARER",
-        'bearer.auth.logical.cluster': TEST_CLUSTER,
-        'bearer.auth.identity.pool.id': TEST_POOL,
-    }
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': "OAUTHBEARER",
+            'bearer.auth.logical.cluster': TEST_CLUSTER,
+            'bearer.auth.identity.pool.id': TEST_POOL}
 
     with pytest.raises(ValueError, match=r"Missing required OAuth configuration properties: (.*)"):
         SchemaRegistryClient(conf)
 
 
 def test_oauth_bearer_config_invalid():
-    conf = {
-        'url': TEST_URL,
-        'bearer.auth.credentials.source': "OAUTHBEARER",
-        'bearer.auth.logical.cluster': TEST_CLUSTER,
-        'bearer.auth.identity.pool.id': 1,
-    }
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': "OAUTHBEARER",
+            'bearer.auth.logical.cluster': TEST_CLUSTER,
+            'bearer.auth.identity.pool.id': 1}
 
     with pytest.raises(TypeError, match=r"identity pool id must be a str, not (.*)"):
         SchemaRegistryClient(conf)
 
-    conf = {
-        'url': TEST_URL,
-        'bearer.auth.credentials.source': "OAUTHBEARER",
-        'bearer.auth.logical.cluster': 1,
-        'bearer.auth.identity.pool.id': TEST_POOL,
-    }
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': "OAUTHBEARER",
+            'bearer.auth.logical.cluster': 1,
+            'bearer.auth.identity.pool.id': TEST_POOL}
 
     with pytest.raises(TypeError, match=r"logical cluster must be a str, not (.*)"):
         SchemaRegistryClient(conf)
 
-    conf = {
-        'url': TEST_URL,
-        'bearer.auth.credentials.source': "OAUTHBEARER",
-        'bearer.auth.logical.cluster': TEST_CLUSTER,
-        'bearer.auth.identity.pool.id': TEST_POOL,
-        'bearer.auth.client.id': 1,
-        'bearer.auth.client.secret': TEST_USER_PASSWORD,
-        'bearer.auth.scope': TEST_SCOPE,
-        'bearer.auth.issuer.endpoint.url': TEST_ENDPOINT,
-    }
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': "OAUTHBEARER",
+            'bearer.auth.logical.cluster': TEST_CLUSTER,
+            'bearer.auth.identity.pool.id': TEST_POOL,
+            'bearer.auth.client.id': 1,
+            'bearer.auth.client.secret': TEST_USER_PASSWORD,
+            'bearer.auth.scope': TEST_SCOPE,
+            'bearer.auth.issuer.endpoint.url': TEST_ENDPOINT}
 
     with pytest.raises(TypeError, match=r"bearer.auth.client.id must be a str, not (.*)"):
         SchemaRegistryClient(conf)
 
-    conf = {
-        'url': TEST_URL,
-        'bearer.auth.credentials.source': "OAUTHBEARER",
-        'bearer.auth.logical.cluster': TEST_CLUSTER,
-        'bearer.auth.identity.pool.id': TEST_POOL,
-        'bearer.auth.client.id': TEST_USERNAME,
-        'bearer.auth.client.secret': 1,
-        'bearer.auth.scope': TEST_SCOPE,
-        'bearer.auth.issuer.endpoint.url': TEST_ENDPOINT,
-    }
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': "OAUTHBEARER",
+            'bearer.auth.logical.cluster': TEST_CLUSTER,
+            'bearer.auth.identity.pool.id': TEST_POOL,
+            'bearer.auth.client.id': TEST_USERNAME,
+            'bearer.auth.client.secret': 1,
+            'bearer.auth.scope': TEST_SCOPE,
+            'bearer.auth.issuer.endpoint.url': TEST_ENDPOINT}
 
     with pytest.raises(TypeError, match=r"bearer.auth.client.secret must be a str, not (.*)"):
         SchemaRegistryClient(conf)
 
-    conf = {
-        'url': TEST_URL,
-        'bearer.auth.credentials.source': "OAUTHBEARER",
-        'bearer.auth.logical.cluster': TEST_CLUSTER,
-        'bearer.auth.identity.pool.id': TEST_POOL,
-        'bearer.auth.client.id': TEST_USERNAME,
-        'bearer.auth.client.secret': TEST_USER_PASSWORD,
-        'bearer.auth.scope': 1,
-        'bearer.auth.issuer.endpoint.url': TEST_ENDPOINT,
-    }
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': "OAUTHBEARER",
+            'bearer.auth.logical.cluster': TEST_CLUSTER,
+            'bearer.auth.identity.pool.id': TEST_POOL,
+            'bearer.auth.client.id': TEST_USERNAME,
+            'bearer.auth.client.secret': TEST_USER_PASSWORD,
+            'bearer.auth.scope': 1,
+            'bearer.auth.issuer.endpoint.url': TEST_ENDPOINT}
 
     with pytest.raises(TypeError, match=r"bearer.auth.scope must be a str, not (.*)"):
         SchemaRegistryClient(conf)
 
-    conf = {
-        'url': TEST_URL,
-        'bearer.auth.credentials.source': "OAUTHBEARER",
-        'bearer.auth.logical.cluster': TEST_CLUSTER,
-        'bearer.auth.identity.pool.id': TEST_POOL,
-        'bearer.auth.client.id': TEST_USERNAME,
-        'bearer.auth.client.secret': TEST_USER_PASSWORD,
-        'bearer.auth.scope': TEST_SCOPE,
-        'bearer.auth.issuer.endpoint.url': 1,
-    }
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': "OAUTHBEARER",
+            'bearer.auth.logical.cluster': TEST_CLUSTER,
+            'bearer.auth.identity.pool.id': TEST_POOL,
+            'bearer.auth.client.id': TEST_USERNAME,
+            'bearer.auth.client.secret': TEST_USER_PASSWORD,
+            'bearer.auth.scope': TEST_SCOPE,
+            'bearer.auth.issuer.endpoint.url': 1}
 
     with pytest.raises(TypeError, match=r"bearer.auth.issuer.endpoint.url must be a str, not (.*)"):
         SchemaRegistryClient(conf)
 
 
 def test_oauth_bearer_config_valid():
-    conf = {
-        'url': TEST_URL,
-        'bearer.auth.credentials.source': "OAUTHBEARER",
-        'bearer.auth.logical.cluster': TEST_CLUSTER,
-        'bearer.auth.identity.pool.id': TEST_POOL,
-        'bearer.auth.client.id': TEST_USERNAME,
-        'bearer.auth.client.secret': TEST_USER_PASSWORD,
-        'bearer.auth.scope': TEST_SCOPE,
-        'bearer.auth.issuer.endpoint.url': TEST_ENDPOINT,
-    }
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': "OAUTHBEARER",
+            'bearer.auth.logical.cluster': TEST_CLUSTER,
+            'bearer.auth.identity.pool.id': TEST_POOL,
+            'bearer.auth.client.id': TEST_USERNAME,
+            'bearer.auth.client.secret': TEST_USER_PASSWORD,
+            'bearer.auth.scope': TEST_SCOPE,
+            'bearer.auth.issuer.endpoint.url': TEST_ENDPOINT}
 
     client = SchemaRegistryClient(conf)
 
-    assert client._rest_client.client_id == TEST_USERNAME
-    assert client._rest_client.client_secret == TEST_USER_PASSWORD
-    assert client._rest_client.scope == TEST_SCOPE
-    assert client._rest_client.token_endpoint == TEST_ENDPOINT
+    assert client._rest_client.bearer_field_provider.client.client_id == TEST_USERNAME
+    assert client._rest_client.bearer_field_provider.client.client_secret == TEST_USER_PASSWORD
+    assert client._rest_client.bearer_field_provider.client.scope == TEST_SCOPE
+    assert client._rest_client.bearer_field_provider.token_endpoint == TEST_ENDPOINT
+
+
+def test_oauth_bearer_azure_imds_config_invalid():
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': "OAUTHBEARER_AZURE_IMDS",
+            'bearer.auth.logical.cluster': TEST_CLUSTER,
+            'bearer.auth.identity.pool.id': 1}
+
+    with pytest.raises(TypeError, match=r"identity pool id must be a str, not (.*)"):
+        SchemaRegistryClient(conf)
+
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': "OAUTHBEARER_AZURE_IMDS",
+            'bearer.auth.logical.cluster': 1,
+            'bearer.auth.identity.pool.id': TEST_POOL}
+
+    with pytest.raises(TypeError, match=r"logical cluster must be a str, not (.*)"):
+        SchemaRegistryClient(conf)
+
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': "OAUTHBEARER_AZURE_IMDS",
+            'bearer.auth.logical.cluster': TEST_CLUSTER,
+            'bearer.auth.identity.pool.id': TEST_POOL,
+            'bearer.auth.issuer.endpoint.url': 1}
+
+    with pytest.raises(TypeError, match=r"bearer.auth.issuer.endpoint.url must be a str, not (.*)"):
+        SchemaRegistryClient(conf)
+
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': "OAUTHBEARER_AZURE_IMDS",
+            'bearer.auth.logical.cluster': TEST_CLUSTER,
+            'bearer.auth.identity.pool.id': TEST_POOL,
+            'bearer.auth.issuer.endpoint.url': 'http://[wrong_url'}
+
+    with pytest.raises(ValueError, match=r"Failed to parse token endpoint URL: (.*)"):
+        SchemaRegistryClient(conf)
+
+    for url in [{'bearer.auth.issuer.endpoint.url': 'http://test'}, {}]:
+        conf = {'url': TEST_URL,
+                'bearer.auth.credentials.source': "OAUTHBEARER_AZURE_IMDS",
+                'bearer.auth.logical.cluster': TEST_CLUSTER,
+                'bearer.auth.identity.pool.id': TEST_POOL,
+                'bearer.auth.issuer.endpoint.query': 1,
+                **url}
+
+        with pytest.raises(TypeError, match=r"bearer.auth.issuer.endpoint.query must be a str, not (.*)"):
+            SchemaRegistryClient(conf)
+
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': "OAUTHBEARER_AZURE_IMDS",
+            'bearer.auth.logical.cluster': TEST_CLUSTER,
+            'bearer.auth.identity.pool.id': TEST_POOL}
+
+    with pytest.raises(ValueError, match=r"bearer.auth.issuer.endpoint.query must be provided (.*)"):
+        SchemaRegistryClient(conf)
+
+
+def test_oauth_bearer_azure_imds_config_valid():
+    expected_token_endpoints = {
+        'http://alias': 'http://alias',
+        '': 'http://169.254.169.254/metadata/identity/oauth2/token'
+    }
+    query = 'resource=api://test&api-version=2018-02-01'
+
+    for url in [{'bearer.auth.issuer.endpoint.url': 'http://alias'}, {}]:
+        conf = {'url': TEST_URL,
+                'bearer.auth.credentials.source': "OAUTHBEARER_AZURE_IMDS",
+                'bearer.auth.logical.cluster': TEST_CLUSTER,
+                'bearer.auth.identity.pool.id': TEST_POOL,
+                'bearer.auth.issuer.endpoint.query': query,
+                **url}
+
+        client = SchemaRegistryClient(conf)
+        if 'bearer.auth.issuer.endpoint.url' in url:
+            expected_token_endpoint = expected_token_endpoints[
+                url['bearer.auth.issuer.endpoint.url']
+            ]
+        else:
+            expected_token_endpoint = expected_token_endpoints['']
+        expected_token_endpoint += f'?{query}'
+
+        assert client._rest_client.bearer_field_provider.token_endpoint == expected_token_endpoint
 
 
 def test_static_bearer_config():
-    conf = {
-        'url': TEST_URL,
-        'bearer.auth.credentials.source': 'STATIC_TOKEN',
-        'bearer.auth.logical.cluster': 'lsrc',
-        'bearer.auth.identity.pool.id': 'pool_id',
-    }
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': 'STATIC_TOKEN',
+            'bearer.auth.logical.cluster': 'lsrc',
+            'bearer.auth.identity.pool.id': 'pool_id'}
 
     with pytest.raises(ValueError, match='Missing bearer.auth.token'):
         SchemaRegistryClient(conf)
 
 
 def test_custom_bearer_config():
-    conf = {'url': TEST_URL, 'bearer.auth.credentials.source': 'CUSTOM'}
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': 'CUSTOM'}
 
     with pytest.raises(ValueError, match='Missing required custom OAuth configuration properties:'):
         SchemaRegistryClient(conf)
@@ -263,12 +334,10 @@ def test_custom_bearer_config_valid():
 
     custom_config = {}
 
-    conf = {
-        'url': TEST_URL,
-        'bearer.auth.credentials.source': 'CUSTOM',
-        'bearer.auth.custom.provider.function': custom_function,
-        'bearer.auth.custom.provider.config': custom_config,
-    }
+    conf = {'url': TEST_URL,
+            'bearer.auth.credentials.source': 'CUSTOM',
+            'bearer.auth.custom.provider.function': custom_function,
+            'bearer.auth.custom.provider.config': custom_config}
 
     client = SchemaRegistryClient(conf)
 
@@ -277,14 +346,12 @@ def test_custom_bearer_config_valid():
 
 
 def test_config_unknown_prop():
-    conf = {
-        'url': TEST_URL,
-        'basic.auth.credentials.source': 'SASL_INHERIT',
-        'sasl.username': 'user_sasl',
-        'sasl.password': 'secret_sasl',
-        'invalid.conf': 1,
-        'invalid.conf2': 2,
-    }
+    conf = {'url': TEST_URL,
+            'basic.auth.credentials.source': 'SASL_INHERIT',
+            'sasl.username': 'user_sasl',
+            'sasl.password': 'secret_sasl',
+            'invalid.conf': 1,
+            'invalid.conf2': 2}
 
     with pytest.raises(ValueError, match=r"Unrecognized properties: (.*)"):
         SchemaRegistryClient(conf)
@@ -301,11 +368,9 @@ def test_config_encrypt_executor():
     # configure with additional rule_conf keys is fine
     executor.configure(client_conf, rule_conf2)
 
-    client_conf2 = {
-        'url': 'mock://',
-        'ssl.key.location': '/ssl/keys/client',
-        'ssl.certificate.location': '/ssl/certs/client',
-    }
+    client_conf2 = {'url': 'mock://',
+                    'ssl.key.location': '/ssl/keys/client',
+                    'ssl.certificate.location': '/ssl/certs/client'}
     with pytest.raises(RuleError, match="executor already configured"):
         executor.configure(client_conf2, rule_conf)
 
