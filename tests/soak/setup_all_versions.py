a2f066da6bbf28d3285b1c6b4764ad68967aac55
Emanuele Sabellico <esabellico@confluent.io>
2026-04-01 18:45:10 +0200

    v2.14.0 (#2227)
    * v2.14.0
    
    * Change librdkafka version to the final one

--- tests/soak/setup_all_versions.py
+++ tests/soak/setup_all_versions.py
@@ -5,7 +5,7 @@ import subprocess
 PYTHON_SOAK_TEST_BRANCH = 'master'
 
 LIBRDKAFKA_VERSIONS = [
-    '2.14.0-RC1',
+    '2.14.0',
     '2.13.2',
     '2.13.0',
     '2.12.1',
@@ -22,7 +22,7 @@ LIBRDKAFKA_VERSIONS = [
 ]
 
 PYTHON_VERSIONS = [
-    '2.14.0rc1',
+    '2.14.0',
     '2.13.2',
     '2.13.0',
     '2.12.1',
